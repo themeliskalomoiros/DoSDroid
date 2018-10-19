8a140f755adaf2db80867b976fcda42b8df1470c
Tomas Michael <tomasmichael995@gmail.com>
2018-10-19 20:53:25 +0300

    Initial commit

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/MainActivity.java
@@ -0,0 +1,13 @@
+package gr.kalymnos.sk3m3l10.ddosdroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
