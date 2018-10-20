29ad7423abbb75fb4bfb13098ae6af5f749223c4
Tomas Michael <tomasmichael995@gmail.com>
2018-10-20 18:06:13 +0300

    Created main_screen.xml and create_attack_screen.xml

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/MainActivity.java
@@ -1,13 +1,20 @@
 package gr.kalymnos.sk3m3l10.ddosdroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.create_attack_screen);
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.network_technologies, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
