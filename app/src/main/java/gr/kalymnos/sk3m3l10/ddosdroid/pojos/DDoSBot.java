52cfa940f664163c0afca22e733c6b2d2223a96b
Tomas Michael <tomasmichael995@gmail.com>
2018-10-20 13:35:36 +0300

    Created DDoSBot.java

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/pojos/DDoSBot.java
@@ -0,0 +1,40 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.pojos;
+
+/*  This class represents a single bot which apparently
+    belongs to a botnet of a DDoSAttack.*/
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class DDoSBot {
+
+    public interface OnJoinedAttackListener {
+        void onJoinedAttackSuccess();
+
+        void onJoinedAttackFail();
+    }
+
+    public interface OnLeftAttackListener {
+        void onLeftAttackSuccess();
+
+        void onLeftAttackFail();
+    }
+
+    private String instanceId;
+
+    public DDoSBot(String instanceId) {
+        this.instanceId = instanceId;
+    }
+
+    public void joinAttack(String attackId, OnJoinedAttackListener listener) {
+
+    }
+
+    public void leaveAttack(String attackId, OnLeftAttackListener listener) {
+
+    }
+
+    public static DDoSBot getLocalUserDDoSBot() {
+        String instanceId = FirebaseInstanceId.getInstance().getId();
+        return new DDoSBot(instanceId);
+    }
+}
