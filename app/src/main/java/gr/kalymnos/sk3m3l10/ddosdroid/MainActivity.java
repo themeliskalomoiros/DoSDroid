3313c47b0bc6615e632320267af566a86d367b09
Tomas Michael <tomasmichael995@gmail.com>
2018-10-21 17:10:14 +0300

    Refactor: renamed all xml resources, first its type and then the name, (ex screen_name or list_item_name)

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/MainActivity.java
@@ -3,7 +3,6 @@ package gr.kalymnos.sk3m3l10.ddosdroid;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,13 +10,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.attack_screen);
+        setContentView(R.layout.screen_attack);
     }
 
     private void populateSpinner(){
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.network_technologies, R.layout.spinner_item);
+                R.array.network_technologies, R.layout.item_spinner);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
     }
