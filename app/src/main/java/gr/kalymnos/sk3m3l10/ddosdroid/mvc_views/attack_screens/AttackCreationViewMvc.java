6a97ad5cc3e436505365f14e38fde4c206f89cbc
Tomas Michael <tomasmichael995@gmail.com>
2018-10-22 17:11:51 +0300

    Created AttackCreationViewMvc.java

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_views/attack_screens/AttackCreationViewMvc.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_views/attack_screens/AttackCreationViewMvc.java
@@ -3,11 +3,18 @@ package gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.attack_screens;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.ViewMvc;
 
 public interface AttackCreationViewMvc extends ViewMvc {
+
     interface OnAttackCreationButtonClickListener {
         void onAttackCreationButtonClicked();
+    }
+
+    interface OnSpinnerItemSelectedListener {
+        void onSpinnerItemSelected(String item);
     }
 
     String getWebsite();
 
     void setOnAttackCreationButtonClickListener(OnAttackCreationButtonClickListener listener);
+
+    void setOnSpinnerItemSelectedListener(OnSpinnerItemSelectedListener listener);
 }
