edc5b2fdba6666f7a22a42a139aaf5b0a0669e34
Tomas Michael <tomasmichael995@gmail.com>
2018-10-23 11:31:07 +0300

    Temporal implementations of websiteTextChanged() and onSpinnerItemSelected() of AttackCreationFragment

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/fragments/AttackCreationFragment.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/fragments/AttackCreationFragment.java
@@ -4,17 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.ViewMvc;
+import gr.kalymnos.sk3m3l10.ddosdroid.R;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.attack_screens.AttackCreationViewMvc;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.attack_screens.AttackCreationViewMvcImpl;
 
 public class AttackCreationFragment extends Fragment implements AttackCreationViewMvc.OnSpinnerItemSelectedListener,
-AttackCreationViewMvc.OnAttackCreationButtonClickListener{
+        AttackCreationViewMvc.OnAttackCreationButtonClickListener, AttackCreationViewMvc.OnWebsiteTextChangeListener {
 
     private AttackCreationViewMvc viewMvc;
 
@@ -31,13 +32,23 @@ AttackCreationViewMvc.OnAttackCreationButtonClickListener{
     }
 
     @Override
-    public void onSpinnerItemSelected(String item) {
-        Toast.makeText(getContext(), "Spinner item selected!", Toast.LENGTH_SHORT).show();
+    public void onSpinnerItemSelected(String hint) {
+        viewMvc.setNetworkConfigHint(hint);
     }
 
     private void initializeViewMvc(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
         viewMvc = new AttackCreationViewMvcImpl(inflater, container);
         viewMvc.setOnAttackCreationButtonClickListener(this);
         viewMvc.setOnSpinnerItemSelectedListener(this);
+        viewMvc.setOnWebsiteTextChangeListener(this);
+    }
+
+    @Override
+    public void websiteTextChanged(String text) {
+        if (TextUtils.isEmpty(text)) {
+            viewMvc.setWebsiteHint(getString(R.string.set_the_target_of_the_attack_label));
+        } else {
+            viewMvc.setWebsiteHint(getString(R.string.target_set_to_label) + " " + text + ".");
+        }
     }
 }
