363619a72561d230e30acdb692caacf4a6c554a5
Tomas Michael <tomasmichael995@gmail.com>
2018-10-23 15:42:18 +0300

    Created bundleContainsKey() for ValidationUtils

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/ValidationUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/ValidationUtils.java
@@ -1,11 +1,20 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.utils;
 
+import android.os.Bundle;
+
 import java.util.List;
 
-public class ListUtils {
+public class ValidationUtils {
 
     public static boolean listHasItems(List list) {
         if (list != null && list.size() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean bundleContainsKey(Bundle bundle, String key) {
+        if (bundle.containsKey(key)) {
             return true;
         }
         return false;
