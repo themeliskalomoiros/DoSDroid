26714b51c58c7fab482186f5cb737b54c7cb06cf
Tomas Michael <tomasmichael995@gmail.com>
2018-10-23 18:04:45 +0300

    AttackPhaseActivity implements the attack fragment communicators

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/AttackPhaseActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/AttackPhaseActivity.java
@@ -3,12 +3,15 @@ package gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.widget.Toast;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.fragments.AttackCreationFragment;
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.fragments.AttackInfoFragment;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.attack_screens.AttackPhaseViewMvc;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.attack_screens.AttackPhaseViewMvcImpl;
 
-public class AttackPhaseActivity extends AppCompatActivity {
+public class AttackPhaseActivity extends AppCompatActivity implements AttackInfoFragment.OnBeginAttackButtonClickListener,
+        AttackCreationFragment.OnAttackCreationButtonClickListener {
 
     private AttackPhaseViewMvc viewMvc;
 
@@ -16,9 +19,21 @@ public class AttackPhaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewMvc = new AttackPhaseViewMvcImpl(LayoutInflater.from(this), null);
-        getSupportFragmentManager().beginTransaction()
-                .add(viewMvc.getFragmentContainerId(), new AttackCreationFragment()).commit();
         setContentView(viewMvc.getRootView());
+        getSupportFragmentManager().beginTransaction()
+                .replace(viewMvc.getFragmentContainerId(), new AttackCreationFragment())
+                .commit();
     }
 
+    @Override
+    public void onAttackCreationButtonClicked() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(viewMvc.getFragmentContainerId(), new AttackInfoFragment())
+                .commit();
+    }
+
+    @Override
+    public void onBeginAttackButtonClick() {
+        Toast.makeText(this, "attack begin", Toast.LENGTH_SHORT).show();
+    }
 }
