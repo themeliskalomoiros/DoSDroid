199eebad466cdb3ed5570cb9a375189f727f1cc6
Tomas Michael <tomasmichael995@gmail.com>
2018-10-23 15:50:06 +0300

    Created AttackInfoFragment.java

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/fragments/AttackInfoFragment.java
@@ -0,0 +1,42 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.attack_screens.AttackInfoViewMvc;
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.attack_screens.AttackInfoViewMvcImpl;
+import gr.kalymnos.sk3m3l10.ddosdroid.utils.ValidationUtils;
+
+public class AttackInfoFragment extends Fragment implements AttackInfoViewMvc.OnBeginAttackButtonClickListener {
+
+    private AttackInfoViewMvc viewMvc;
+    private boolean websiteTextExists;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        initializeViewMvc(inflater, container);
+        return viewMvc.getRootView();
+    }
+
+    private void initializeViewMvc(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+        viewMvc = new AttackInfoViewMvcImpl(inflater, container);
+        viewMvc.setOnBeginAttacButtonClickListener(this);
+        websiteTextExists = getArguments() != null && ValidationUtils.bundleContainsKey(getArguments(),
+                AttackInfoViewMvc.WEBSITE_KEY);
+        if (websiteTextExists) {
+            viewMvc.bindWebsite(getArguments().getString(AttackInfoViewMvc.WEBSITE_KEY));
+        }
+    }
+
+    @Override
+    public void onBeginAttackButtonClick() {
+        Toast.makeText(getContext(), "Fab clicked", Toast.LENGTH_SHORT).show();
+    }
+}
