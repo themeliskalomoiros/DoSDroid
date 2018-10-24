57d977658a488e1277ffb169fffd1f0bf9d498c7
Tomas Michael <tomasmichael995@gmail.com>
2018-10-24 21:46:41 +0300

    AttackListActivity initializes its mvc view and fetches DDoSAttack's through its AttackRepository

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/AttackListActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/AttackListActivity.java
@@ -2,11 +2,18 @@ package gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.widget.Toast;
+
+import java.util.List;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.AttackRepository;
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.FakeAttackRepo;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.attack_list_screen.AttackListViewMvc;
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.attack_list_screen.AttackListViewMvcImpl;
+import gr.kalymnos.sk3m3l10.ddosdroid.pojos.DDoSAttack;
 
-public class AttackListActivity extends AppCompatActivity {
+public class AttackListActivity extends AppCompatActivity implements AttackRepository.OnAttacksFetchListener {
 
     private AttackListViewMvc viewMvc;
     private AttackRepository attackRepo;
@@ -14,5 +21,39 @@ public class AttackListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        viewMvc = new AttackListViewMvcImpl(LayoutInflater.from(this), null);
+        attackRepo = new FakeAttackRepo();
+        setContentView(viewMvc.getRootView());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        attackRepo.registerOnAttacksFetchListener(this);
+        attackRepo.fetchAllAttacks();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        attackRepo.unRegisterOnAttacksFetchListener();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        attackRepo = null;
+    }
+
+    @Override
+    public void attacksFetchedSuccess(List<DDoSAttack> attacks) {
+        //  This method was called from a worker thread and as we know
+        //  the ui must always be updated through the main (UI) thread
+        runOnUiThread(() -> viewMvc.bindAttacks(attacks));
+    }
+
+    @Override
+    public void attacksFetchedFail(String msg) {
+        //  TODO: Display the error somewhere besides the toast
     }
 }
