ec0c408204c4f93ec8d9bd6ecce7024a813cf345
Tomas Michael <tomasmichael995@gmail.com>
2018-10-24 21:45:49 +0300

    MainActivity starts AttackListActivity when the proper item is clicked

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/MainActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/MainActivity.java
@@ -32,6 +32,6 @@ public class MainActivity extends AppCompatActivity implements MainScreenViewMvc
 
     @Override
     public void onFollowingAttacksClick() {
-        Toast.makeText(this, "onfollowingattacks", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this,AttackListActivity.class));
     }
 }
