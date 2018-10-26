1186ca176db89e0df3afd3fda9ef5ce91e759631
Tomas Michael <tomasmichael995@gmail.com>
2018-10-26 14:40:27 +0300

    All attack type keys moved to DDoSAttack class

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/pojos/DDoSAttack.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/pojos/DDoSAttack.java
@@ -12,6 +12,12 @@ import static gr.kalymnos.sk3m3l10.ddosdroid.utils.ValidationUtils.listHasItems;
 public class DDoSAttack implements Parcelable {
 
     private static final String TAG = DDoSAttack.class.getSimpleName();
+    public static final String ATTACK_TYPE_KEY = TAG + "attack type key";
+    public static final String CACHED_ATTACKS_KEY = TAG + "caching attacks key";
+    public static final int TYPE_FETCH_ALL = 101;
+    public static final int TYPE_FETCH_FOLLOWING = 102;
+    public static final int TYPE_FETCH_OWNER = 103;
+    public static final int TYPE_NONE = -1;
 
     private String pushId, targetWebsite;
     private List<DDoSBot> botsList;
