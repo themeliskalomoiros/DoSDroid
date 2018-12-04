521ed004d5dd3711b39ac87035b8f2965c196239
Tomas Michael <tomasmichael995@gmail.com>
2018-12-04 23:25:12 +0200

    Created JoinAttackScreen

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_views/screen_join_attack/JoinAttackScreen.java
@@ -0,0 +1,20 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.ViewMvcWithToolbar;
+
+public interface JoinAttackScreen extends ViewMvcWithToolbar {
+    interface OnJoinAttackClickListener {
+        void onJoinAttackClicked();
+    }
+
+    void OnJoinAttackClickListener(OnJoinAttackClickListener listener);
+
+    void bindWebsite(String website);
+
+    void bindWebsiteDate(long dateMilli);
+
+    void bindAttackForce(int count);
+
+    void bindNetworkConfiguration(String networkConf);
+
+}
