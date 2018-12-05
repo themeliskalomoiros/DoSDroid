efdfed2046f389b02e7b3b535c5a83441ef0fa1c
Tomas Michael <tomasmichael995@gmail.com>
2018-12-05 14:12:55 +0200

    Removed toolbar from JoinAttackInfoViewMvc

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_views/screen_join_attack/JoinAttackInfoViewMvc.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_views/screen_join_attack/JoinAttackInfoViewMvc.java
@@ -1,8 +1,8 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack;
 
-import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.ViewMvcWithToolbar;
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.ViewMvc;
 
-public interface JoinAttackInfoViewMvc extends ViewMvcWithToolbar {
+public interface JoinAttackInfoViewMvc extends ViewMvc {
     interface OnJoinAttackClickListener {
         void onJoinAttackClicked();
     }
--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_views/screen_join_attack/JoinAttackInfoViewMvcImp.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_views/screen_join_attack/JoinAttackInfoViewMvcImp.java
@@ -1,7 +1,6 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack;
 
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +11,6 @@ import gr.kalymnos.sk3m3l10.ddosdroid.R;
 public class JoinAttackInfoViewMvcImp implements JoinAttackInfoViewMvc {
 
     private View root;
-    private Toolbar toolbar;
     private TextView website, date, attackForce, networkConf;
     private FloatingActionButton joinFab;
 
@@ -50,18 +48,12 @@ public class JoinAttackInfoViewMvcImp implements JoinAttackInfoViewMvc {
     }
 
     @Override
-    public Toolbar getToolbar() {
-        return toolbar;
-    }
-
-    @Override
     public View getRootView() {
         return root;
     }
 
     private void initializeViews(LayoutInflater inflater, ViewGroup container) {
         root = inflater.inflate(R.layout.screen_join_attack_info, container, false);
-        toolbar = root.findViewById(R.id.toolBar);
         website = root.findViewById(R.id.website_textview);
         date = root.findViewById(R.id.tv_website_hint);
         attackForce = root.findViewById(R.id.tv_attack_force);
