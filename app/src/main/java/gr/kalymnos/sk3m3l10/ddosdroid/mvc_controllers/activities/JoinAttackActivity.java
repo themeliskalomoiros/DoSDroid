c7dff1ac9f913dbb00a8aa1ee27ff4c0b1d30368
Tomas Michael <tomasmichael995@gmail.com>
2018-12-05 15:00:49 +0200

    JoinAttackActivity starts by showing a JoinAttackInfoFragment

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/JoinAttackActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/JoinAttackActivity.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.fragments.JoinAttackInfoFragment;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack.JoinAttackViewMvc;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack.JoinAttackViewMvcImp;
+import gr.kalymnos.sk3m3l10.ddosdroid.pojos.DDoSAttack;
+
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.DDoSAttack.Extra.EXTRA_ATTACK;
 
 public class JoinAttackActivity extends AppCompatActivity {
 
@@ -15,11 +19,18 @@ public class JoinAttackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupUi();
+        showJoinAttackInfoFragment();
     }
 
     private void setupUi() {
         viewMvc = new JoinAttackViewMvcImp(LayoutInflater.from(this), null);
         setSupportActionBar(viewMvc.getToolbar());
         setContentView(viewMvc.getRootView());
+    }
+
+    private void showJoinAttackInfoFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(viewMvc.getFragmentContainerId(),JoinAttackInfoFragment.getInstance(getIntent().getExtras()))
+                .commit();
     }
 }
