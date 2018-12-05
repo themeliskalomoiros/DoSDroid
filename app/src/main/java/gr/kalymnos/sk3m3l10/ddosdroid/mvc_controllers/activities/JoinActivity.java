03c15e14ad9e69e11a25ee3cf9297deda3c5c709
Tomas Michael <tomasmichael995@gmail.com>
2018-12-05 14:22:10 +0200

    Created JoinActivity

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/JoinActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/JoinActivity.java
@@ -1,16 +1,25 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack.JoinAttackViewMvc;
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack.JoinAttackViewMvcImp;
 
 public class JoinActivity extends AppCompatActivity {
 
-    private JoinAttackViewMvc
+    private JoinAttackViewMvc viewMvc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupUi();
+    }
+
+    private void setupUi() {
+        viewMvc = new JoinAttackViewMvcImp(LayoutInflater.from(this), null);
+        setSupportActionBar(viewMvc.getToolbar());
+        setContentView(viewMvc.getRootView());
     }
 }
