b89cb510cdd850bc212c0f281f218e5b523dd462
Tomas Michael <tomasmichael995@gmail.com>
2018-12-05 15:23:20 +0200

    Removed some listener definisions inside bot class

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/pojos/Bot.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/pojos/Bot.java
@@ -1,24 +1,8 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.pojos;
 
-/*  This class represents a single bot which apparently
-    belongs to a botnet of a Attack.*/
-
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.instance_id_provider.FakeInstanceIdProvider;
 
 public class Bot {
-
-    public interface OnJoinedAttackListener {
-        void onJoinedAttackSuccess();
-
-        void onJoinedAttackFail();
-    }
-
-    public interface OnLeftAttackListener {
-        void onLeftAttackSuccess();
-
-        void onLeftAttackFail();
-    }
-
     private String id;
 
     public Bot(String instanceId) {
@@ -27,14 +11,6 @@ public class Bot {
 
     public String getId() {
         return id;
-    }
-
-    public void joinAttack(String attackId, OnJoinedAttackListener listener) {
-
-    }
-
-    public void leaveAttack(String attackId, OnLeftAttackListener listener) {
-
     }
 
     public static Bot getLocalUserDDoSBot() {
