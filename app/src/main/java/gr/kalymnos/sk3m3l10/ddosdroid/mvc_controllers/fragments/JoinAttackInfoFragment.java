da6f7e20bfa137b7b7903262ea8477cc60190ece
Tomas Michael <tomasmichael995@gmail.com>
2018-12-05 15:02:48 +0200

    JoinAttackInfoFragment is displaying a JoinAttackInfoViewMvc

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/fragments/JoinAttackInfoFragment.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/fragments/JoinAttackInfoFragment.java
@@ -1,20 +1,40 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack.JoinAttackInfoViewMvc;
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack.JoinAttackInfoViewMvcImp;
 
 public class JoinAttackInfoFragment extends Fragment implements JoinAttackInfoViewMvc.OnJoinAttackClickListener {
 
-    public static JoinAttackInfoFragment getInstance(Bundle args) {
-        JoinAttackInfoFragment instance = new JoinAttackInfoFragment();
-        instance.setArguments(args);
-        return instance;
+    private JoinAttackInfoViewMvc viewMvc;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        initializeViewMvc(inflater, container);
+        return viewMvc.getRootView();
     }
 
     @Override
     public void onJoinAttackClicked() {
 
+    }
+
+    private void initializeViewMvc(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+        viewMvc = new JoinAttackInfoViewMvcImp(inflater, container);
+        viewMvc.setOnJoinAttackClickListener(this);
+    }
+
+    public static JoinAttackInfoFragment getInstance(Bundle args) {
+        JoinAttackInfoFragment instance = new JoinAttackInfoFragment();
+        instance.setArguments(args);
+        return instance;
     }
 }
