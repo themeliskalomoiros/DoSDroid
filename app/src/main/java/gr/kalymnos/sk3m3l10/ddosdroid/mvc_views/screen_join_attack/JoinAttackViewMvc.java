a105ca7501ac9967a3161dcd1fddb85ad3c190ee
Tomas Michael <tomasmichael995@gmail.com>
2018-12-05 14:19:26 +0200

    Created JoinAttackViewMvc/Imp

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_views/screen_join_attack/JoinAttackViewMvc.java
@@ -0,0 +1,7 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.ViewMvcWithToolbar;
+
+public interface JoinAttackViewMvc extends ViewMvcWithToolbar {
+    int getFragmentContainerId();
+}
---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_views/screen_join_attack/JoinAttackViewMvcImp.java
@@ -0,0 +1,41 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack;
+
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.R;
+
+public class JoinAttackViewMvcImp implements JoinAttackViewMvc {
+
+    private View root;
+    private Toolbar toolbar;
+    private FrameLayout fragmentContainer;
+
+    public JoinAttackViewMvcImp(LayoutInflater inflater, ViewGroup container) {
+        initializeViews(inflater, container);
+    }
+
+    @Override
+    public int getFragmentContainerId() {
+        return fragmentContainer.getId();
+    }
+
+    @Override
+    public Toolbar getToolbar() {
+        return toolbar;
+    }
+
+    @Override
+    public View getRootView() {
+        return root;
+    }
+
+    private void initializeViews(LayoutInflater inflater, ViewGroup container) {
+        root = inflater.inflate(R.layout.screen_join_attack, container, false);
+        toolbar = root.findViewById(R.id.toolBar);
+        fragmentContainer = root.findViewById(R.id.fragment_container);
+    }
+}
