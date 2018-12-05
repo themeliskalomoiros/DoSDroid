59aa3b2acb030e50feb5c35cd2b6536e6ae6d8e8
Tomas Michael <tomasmichael995@gmail.com>
2018-12-05 20:23:34 +0200

    Created DateFormatter

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/DateFormatter.java
@@ -0,0 +1,21 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.utils;
+
+import android.content.res.Configuration;
+import android.support.v4.os.ConfigurationCompat;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateFormatter {
+
+    public static String getDate(Configuration configuration, long timeMillis) {
+        Date today = new Date(timeMillis);
+        DateFormat dateFormatter = DateFormat.getDateInstance(DateFormat.DEFAULT, getCurrentLocale(configuration));
+        return dateFormatter.format(today);
+    }
+
+    private static Locale getCurrentLocale(Configuration configuration){
+        return ConfigurationCompat.getLocales(configuration).get(0);
+    }
+}
