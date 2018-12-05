f27d2ce04119c5ea6cd6751fb2007a7612803a98
Tomas Michael <tomasmichael995@gmail.com>
2018-12-05 15:18:22 +0200

    JoinAttackInfoFragment binds attack to the ui

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/fragments/JoinAttackInfoFragment.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/fragments/JoinAttackInfoFragment.java
@@ -10,21 +10,37 @@ import android.view.ViewGroup;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack.JoinAttackInfoViewMvc;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack.JoinAttackInfoViewMvcImp;
+import gr.kalymnos.sk3m3l10.ddosdroid.pojos.DDoSAttack;
 
 public class JoinAttackInfoFragment extends Fragment implements JoinAttackInfoViewMvc.OnJoinAttackClickListener {
 
     private JoinAttackInfoViewMvc viewMvc;
+    private DDoSAttack attack;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        attack = getArguments().getParcelable(DDoSAttack.Extra.EXTRA_ATTACK);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         initializeViewMvc(inflater, container);
+        bindAttackToUi();
         return viewMvc.getRootView();
     }
 
     @Override
     public void onJoinAttackClicked() {
 
+    }
+
+    private void bindAttackToUi() {
+        viewMvc.bindAttackForce(attack.getBotNetCount());
+        viewMvc.bindNetworkConfiguration(DDoSAttack.NetworkTypeTranslator.translate(attack.getNetworkType()));
+        viewMvc.bindWebsite(attack.getTargetWebsite());
+        viewMvc.bindWebsiteDate(attack.getTimeMilli());
     }
 
     private void initializeViewMvc(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
