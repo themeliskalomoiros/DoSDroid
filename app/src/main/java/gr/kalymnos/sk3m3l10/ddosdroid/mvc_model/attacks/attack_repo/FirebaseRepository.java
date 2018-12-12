c79fb4e4d42a7c56011f7982283a0703c3c94bb9
Tomas Michael <tomasmichael995@gmail.com>
2018-12-12 20:22:37 +0200

    Implemented FirebaseRepository.fetchAllAttacks()

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attacks/attack_repo/FirebaseRepository.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attacks/attack_repo/FirebaseRepository.java
@@ -1,22 +1,51 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attacks.attack_repo;
 
+import android.support.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.Attack;
 
 public class FirebaseRepository extends AttackRepository {
     private static final String NODE_ATTACKS = "attacks";
 
-    private DatabaseReference rootRef;
+    private DatabaseReference rootRef, attacksRef;
 
     public FirebaseRepository() {
         this.rootRef = FirebaseDatabase.getInstance().getReference();
+        this.attacksRef = rootRef.child(NODE_ATTACKS);
     }
 
     @Override
     public void fetchAllAttacks() {
+        attacksRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<Attack> attacks = extractAttacksFrom(dataSnapshot);
+                callback.attacksFetchedSuccess(attacks);
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                callback.attacksFetchedFail(databaseError.getMessage());
+            }
+
+            @NonNull
+            private List<Attack> extractAttacksFrom(@NonNull DataSnapshot dataSnapshot) {
+                List<Attack> attacks = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    attacks.add(snapshot.getValue(Attack.class));
+                }
+                return attacks;
+            }
+        });
     }
 
     @Override
