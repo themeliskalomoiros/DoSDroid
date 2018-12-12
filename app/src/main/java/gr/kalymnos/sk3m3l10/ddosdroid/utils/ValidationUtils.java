b997c505629475bc8413f7b074b88dc1de529268
Tomas Michael <tomasmichael995@gmail.com>
2018-12-12 17:14:11 +0200

    Created ValidationUtils.mapHasItems()

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/ValidationUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/ValidationUtils.java
@@ -3,11 +3,19 @@ package gr.kalymnos.sk3m3l10.ddosdroid.utils;
 import android.os.Bundle;
 
 import java.util.List;
+import java.util.Map;
 
 public class ValidationUtils {
 
     public static boolean listHasItems(List list) {
         if (list != null && list.size() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean mapHasItems(Map map) {
+        if (map != null && map.size() > 0) {
             return true;
         }
         return false;
