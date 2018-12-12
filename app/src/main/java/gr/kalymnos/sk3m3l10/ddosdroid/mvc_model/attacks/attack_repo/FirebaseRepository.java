f263b3e9223420cdef0374889f83a2401799bd94
Tomas Michael <tomasmichael995@gmail.com>
2018-12-12 20:01:50 +0200

    Created FirebaseRepository

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attacks/attack_repo/FirebaseRepository.java
@@ -0,0 +1,53 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attacks.attack_repo;
+
+import android.app.Activity;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.pojos.Attack;
+
+public class FirebaseRepository extends AttackRepository {
+
+    @Override
+    public void fetchAllAttacks() {
+
+    }
+
+    @Override
+    public void fetchAllAttacksOf(int networkType) {
+
+    }
+
+    @Override
+    public void fetchJoinedAttakcsOf(String botId) {
+
+    }
+
+    @Override
+    public void fetchJoinedAttakcsOf(String botId, int networkType) {
+
+    }
+
+    @Override
+    public void fetchNotJoinedAttacksOf(String botId) {
+
+    }
+
+    @Override
+    public void fetchNotJoinedAttacksOf(String botId, int networkType) {
+
+    }
+
+    @Override
+    public void fetchLocalOwnerAttacks() {
+
+    }
+
+    @Override
+    public void fetchLocalOwnerAttacksOf(int networkType) {
+
+    }
+
+    @Override
+    public void uploadAttack(Attack attack) {
+
+    }
+}
