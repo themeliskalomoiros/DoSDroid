7d9702d8491a19160803af51d6a569d7e5a4b998
Tomas Michael <tomasmichael995@gmail.com>
2018-12-12 20:10:34 +0200

    Added constructor to FirebaseRepository

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attacks/attack_repo/FirebaseRepository.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attacks/attack_repo/FirebaseRepository.java
@@ -1,10 +1,18 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attacks.attack_repo;
 
-import android.app.Activity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.Attack;
 
 public class FirebaseRepository extends AttackRepository {
+    private static final String NODE_ATTACKS = "attacks";
+
+    private DatabaseReference rootRef;
+
+    public FirebaseRepository() {
+        this.rootRef = FirebaseDatabase.getInstance().getReference();
+    }
 
     @Override
     public void fetchAllAttacks() {
