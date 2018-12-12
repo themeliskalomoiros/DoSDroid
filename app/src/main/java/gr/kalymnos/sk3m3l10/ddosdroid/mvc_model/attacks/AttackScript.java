4865cc9c594e535d4d0b3012741ea74fbd4df67b
Tomas Michael <tomasmichael995@gmail.com>
2018-12-12 16:48:43 +0200

    Closing the inputstream when the url done with reading in AttackScript

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attacks/AttackScript.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attacks/AttackScript.java
@@ -3,6 +3,7 @@ package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attacks;
 import android.util.Log;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -25,10 +26,21 @@ public class AttackScript implements Runnable {
     }
 
     private void readFromUrl(URL url) {
+        InputStream in = null;
         try {
-            url.openStream();
+            in = url.openStream();
         } catch (IOException e) {
             Log.e(TAG, "openStream() error.", e);
+        } finally {
+            closeInputStream(in);
+        }
+    }
+
+    private void closeInputStream(InputStream in) {
+        try {
+            in.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Error while closing the input stream.", e);
         }
     }
 
