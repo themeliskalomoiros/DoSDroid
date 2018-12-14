ab4935e794ea4855628d86e7e496d0ccfa11a888
Tomas Michael <tomasmichael995@gmail.com>
2018-12-14 12:12:48 +0200

    Created JoinAttackManager.Builder and its implementation but so far it returns only null instances of JoinAttackManager

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attacks/attack_join_management/JoinAttackManager.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attacks/attack_join_management/JoinAttackManager.java
@@ -3,15 +3,18 @@ package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attacks.attack_join_management;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attacks.attack_network.AttackNetwork;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.Attack;
 
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.AttackConstants.NetworkType.BLUETOOTH;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.AttackConstants.NetworkType.INTERNET;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.AttackConstants.NetworkType.NSD;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.AttackConstants.NetworkType.WIFI_P2P;
+
+
 public abstract class JoinAttackManager {
+    private static final String TAG = "JoinAttackManager";
+
     protected OnJoinAttackListener onJoinAttackListener;
 
-    private JoinAttackManager(){}
-
-    public interface OnJoinAttackListener {
-        void onAttackJoined();
-
-        void onAttackJoinedFailed(String reason);
+    private JoinAttackManager() {
     }
 
     public abstract void joinAttack(Attack attack, AttackNetwork network);
@@ -22,5 +25,37 @@ public abstract class JoinAttackManager {
 
     public void removeOnJoinAttackListener() {
         onJoinAttackListener = null;
+    }
+
+    public interface OnJoinAttackListener {
+        void onAttackJoined();
+
+        void onAttackJoinedFailed(String reason);
+    }
+
+    public interface Builder {
+        public JoinAttackManager build(int attackNetworkType);
+    }
+
+    public static class BuilderImp implements Builder {
+
+        @Override
+        public JoinAttackManager build(int attackNetworkType) {
+            switch (attackNetworkType) {
+                case INTERNET:
+                    return null;
+                case WIFI_P2P:
+                    //  TODO: need to implement
+                    return null;
+                case NSD:
+                    //  TODO: need to implement
+                    return null;
+                case BLUETOOTH:
+                    //  TODO: need to implement
+                    return null;
+                default:
+                    throw new UnsupportedOperationException(TAG + ": Unknown attack network type.");
+            }
+        }
     }
 }
