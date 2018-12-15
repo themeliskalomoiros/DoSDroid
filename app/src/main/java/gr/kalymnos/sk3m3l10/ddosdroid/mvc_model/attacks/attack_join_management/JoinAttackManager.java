1022118f21ad19bd0db7cff438e15a6aae951bf4
Tomas Michael <tomasmichael995@gmail.com>
2018-12-15 21:14:26 +0200

    Deleted JoinAttackManager.OnJoinAttackListener and .Builder

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attacks/attack_join_management/JoinAttackManager.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attacks/attack_join_management/JoinAttackManager.java
@@ -12,51 +12,10 @@ import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.AttackConstants.NetworkType.W
 public abstract class JoinAttackManager {
     private static final String TAG = "JoinAttackManager";
 
-    public interface OnJoinAttackListener {
-        void onAttackJoined();
-
-        void onAttackJoinedFailed(String reason);
+    public JoinAttackManager() {
     }
 
-    protected OnJoinAttackListener onJoinAttackListener;
+    public void joinAttack(Attack attack){
 
-    protected JoinAttackManager() {
-    }
-
-    public abstract void joinAttack(Attack attack, AttackNetwork network);
-
-    public void addOnJoinAttackListener(OnJoinAttackListener listener) {
-        onJoinAttackListener = listener;
-    }
-
-    public void removeOnJoinAttackListener() {
-        onJoinAttackListener = null;
-    }
-
-
-    public interface Builder {
-        public JoinAttackManager build(int attackNetworkType);
-    }
-
-    public static class BuilderImp implements Builder {
-
-        @Override
-        public JoinAttackManager build(int attackNetworkType) {
-            switch (attackNetworkType) {
-                case INTERNET:
-                    return null;
-                case WIFI_P2P:
-                    //  TODO: need to implement
-                    return null;
-                case NSD:
-                    //  TODO: need to implement
-                    return null;
-                case BLUETOOTH:
-                    //  TODO: need to implement
-                    return null;
-                default:
-                    throw new UnsupportedOperationException(TAG + ": Unknown attack network type.");
-            }
-        }
     }
 }
