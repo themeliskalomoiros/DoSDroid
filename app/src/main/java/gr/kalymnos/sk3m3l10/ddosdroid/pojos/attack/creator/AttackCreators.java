b87f5ba1362d563ac04b315630f7fae6b3dbf942
Tomas Michael <tomasmichael995@gmail.com>
2018-12-18 19:23:59 +0200

    Created AttackCreators.getClassFrom()

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/pojos/attack/creator/AttackCreators.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/pojos/attack/creator/AttackCreators.java
@@ -3,10 +3,32 @@ package gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.creator;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.instance_id.FirebaseInstanceId;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.instance_id.InstanceIdProvider;
 
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.BLUETOOTH;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.INTERNET;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.NSD;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.WIFI_P2P;
+
 public final class AttackCreators {
-    public static AttackCreator getLocalAttackCreatorInstance(int networkType){
+    private static final String TAG = "AttackCreators";
+
+    public static AttackCreator getLocalAttackCreatorInstance(int networkType) {
         InstanceIdProvider idProvider = new FirebaseInstanceId();
         String uuid = idProvider.getInstanceId();
-        return new AttackCreatorFactoryImp().build(networkType,uuid);
+        return new AttackCreatorFactoryImp().build(networkType, uuid);
+    }
+
+    public static Class<? extends AttackCreator> getClassFrom(int networkType) {
+        switch (networkType) {
+            case INTERNET:
+                return InternetCreator.class;
+            case BLUETOOTH:
+                return BluetoothCreator.class;
+            case WIFI_P2P:
+                return WifiP2pCreator.class;
+            case NSD:
+                return NsdCreator.class;
+            default:
+                throw new IllegalArgumentException(TAG + ": Unknown attack network type");
+        }
     }
 }
