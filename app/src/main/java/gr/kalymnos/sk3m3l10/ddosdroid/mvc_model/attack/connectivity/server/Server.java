dac1cb3124f00bd78df23a4064c9f05e2aab552b
Tomas Michael <tomasmichael995@gmail.com>
2018-12-19 16:59:30 +0200

    Server accepts an AttackCreator in its constructor

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/Server.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/Server.java
@@ -1,4 +1,11 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server;
 
+import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.creator.AttackCreator;
+
 public abstract class Server {
+    private AttackCreator attackCreator;
+
+    public Server(AttackCreator attackCreator) {
+        this.attackCreator = attackCreator;
+    }
 }
