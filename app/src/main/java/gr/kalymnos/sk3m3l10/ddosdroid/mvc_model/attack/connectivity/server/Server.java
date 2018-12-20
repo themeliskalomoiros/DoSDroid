2a3a94868926ebab5344dd03f096c4feb2ada8bb
Tomas Michael <tomasmichael995@gmail.com>
2018-12-20 18:32:53 +0200

    Created Server.Builder

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/Server.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/Server.java
@@ -1,18 +1,44 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
-import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.creator.HostInfo;
+
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.BLUETOOTH;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.INTERNET;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.NSD;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.WIFI_P2P;
 
 public abstract class Server {
+    private static final String TAG = "Server";
     private Attack attack;
-    private HostInfo hostInfo;
 
-    public Server(Attack attack, HostInfo hostInfo) {
+    public Server(Attack attack) {
         this.attack = attack;
-        this.hostInfo = hostInfo;
     }
 
-    public final String getId(){
+    public final String getId() {
         return attack.getPushId();
+    }
+
+    public interface Builder {
+        Server build(Attack attack);
+    }
+
+    public static class BuilderImp implements Server.Builder {
+
+        @Override
+        public Server build(Attack attack) {
+            switch (attack.getNetworkType()) {
+                case INTERNET:
+                    return new InternetServer(attack);
+                case BLUETOOTH:
+                    return new BluetoothServer(attack);
+                case WIFI_P2P:
+                    return new WifiP2pServer(attack);
+                case NSD:
+                    return new NsdServer(attack);
+                default:
+                    throw new IllegalArgumentException(TAG + ": Unknown attack network type");
+            }
+        }
     }
 }
