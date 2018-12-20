64990d4b27f6daabbc6e7894e53843dda0618764
Tomas Michael <tomasmichael995@gmail.com>
2018-12-20 18:10:37 +0200

    Added attack field to Server

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/Server.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/Server.java
@@ -1,11 +1,18 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server;
 
+import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.creator.HostInfo;
 
 public abstract class Server {
+    private Attack attack;
     private HostInfo hostInfo;
 
-    public Server(HostInfo hostInfo) {
+    public Server(Attack attack, HostInfo hostInfo) {
+        this.attack = attack;
         this.hostInfo = hostInfo;
+    }
+
+    public final String getId(){
+        return attack.getPushId();
     }
 }
