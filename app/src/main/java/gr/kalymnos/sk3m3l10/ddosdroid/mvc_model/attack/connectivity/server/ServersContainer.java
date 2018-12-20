6efa1c752a62ca61a3bc6f0a81b12995ee559ee9
Tomas Michael <tomasmichael995@gmail.com>
2018-12-20 18:13:57 +0200

    ServersContainer has a Map<String,Server> field

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/ServersContainer.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/ServersContainer.java
@@ -4,9 +4,14 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ServersContainer extends Service {
+    private Map<String, Server> serversMap;
 
     public ServersContainer() {
+        serversMap = new HashMap<>();
     }
 
     @Override
