80534c0be021060f6d4a026643b50e9289b1f596
Tomas Michael <tomasmichael995@gmail.com>
2018-12-20 18:18:32 +0200

    ServersContainer handles the start server action in onStartCommand()

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/ServersContainer.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/ServersContainer.java
@@ -8,10 +8,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ServersContainer extends Service {
+    private static final String TAG = "ServersContainer";
+    public static final String ACTION_START_SERVER = TAG + "start server action";
+
     private Map<String, Server> serversMap;
 
     public ServersContainer() {
         serversMap = new HashMap<>();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        switch (intent.getAction()) {
+            case ACTION_START_SERVER:
+                handleStartServerAction(intent);
+                return START_STICKY;
+            default:
+                return super.onStartCommand(intent, flags, startId);
+        }
+    }
+
+    private void handleStartServerAction(Intent intent) {
+        // TODO: implementation needed
     }
 
     @Override
