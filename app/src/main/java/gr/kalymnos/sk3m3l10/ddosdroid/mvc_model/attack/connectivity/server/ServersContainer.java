1cce3de75107057fa670829f98c496c5ffbc26ea
Tomas Michael <tomasmichael995@gmail.com>
2018-12-20 18:10:49 +0200

    Created ServersContainer Service

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/ServersContainer.java
@@ -0,0 +1,16 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ServersContainer extends Service {
+
+    public ServersContainer() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
