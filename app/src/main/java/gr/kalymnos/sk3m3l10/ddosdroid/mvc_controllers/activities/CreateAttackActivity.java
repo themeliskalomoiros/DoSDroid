eebbdee7f0bfd239f793bb27c8479e1daf752f2f
Tomas Michael <tomasmichael995@gmail.com>
2018-12-21 17:07:59 +0200

    Deleted AttackInfoFragment, CreateAttackActivity starts a server when an attack is created (uploaded)

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/CreateAttackActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/CreateAttackActivity.java
@@ -3,16 +3,14 @@ package gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
-import android.widget.Toast;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.fragments.AttackCreationFragment;
-import gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.fragments.AttackInfoFragment;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.ServersHost;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_attack_phase.CreateAttackViewMvc;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_attack_phase.CreateAttackViewMvcImpl;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
 
-public class CreateAttackActivity extends AppCompatActivity implements AttackInfoFragment.OnBeginAttackButtonClickListener,
+public class CreateAttackActivity extends AppCompatActivity implements
         AttackCreationFragment.OnAttackCreationListener {
 
     private CreateAttackViewMvc viewMvc;
@@ -27,16 +25,9 @@ public class CreateAttackActivity extends AppCompatActivity implements AttackInf
     }
 
     @Override
-    public void onBeginAttackButtonClick(Attack attack) {
-        ServersHost.Action.startServer(this,attack);
-        finish();
-    }
-
-    @Override
     public void onAttackCreated(Attack attack) {
-        getSupportFragmentManager().beginTransaction()
-                .replace(viewMvc.getFragmentContainerId(), AttackInfoFragment.Builder.build(attack))
-                .commit();
+        ServersHost.Action.startServer(this, attack);
+        finish();
     }
 
     private void setupUi() {
