4752606aa3c0128fb4f0806fb54a19cdb232795e
Tomas Michael <tomasmichael995@gmail.com>
2018-12-23 23:59:49 +0200

    When client is connected to server then the join procedure starts

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/JoinAttackActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/JoinAttackActivity.java
@@ -55,7 +55,7 @@ public class JoinAttackActivity extends AppCompatActivity implements Client.OnCo
 
     @Override
     public void onAttackNetworkConnected() {
-
+        startJoinProcedure();
     }
 
     @Override
