6e041e3002bf4dc93a05d81846851c6248083876
Tomas Michael <tomasmichael995@gmail.com>
2018-12-27 14:21:29 +0200

    Created NetworkConstraintsResolver.Builder class

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/constraints/NetworkConstraintsResolver.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/constraints/NetworkConstraintsResolver.java
@@ -5,7 +5,14 @@ import android.content.Context;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.BLUETOOTH;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.INTERNET;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.NSD;
+import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.WIFI_P2P;
+
 public class NetworkConstraintsResolver {
+    private static final String TAG = "NetworkConstraintsResol";
+
     private Queue<NetworkConstraint> constraints;
 
     private NetworkConstraintsResolver() {
@@ -17,5 +24,42 @@ public class NetworkConstraintsResolver {
             constraints.poll().resolve(context);
     }
 
-    // TODO: Create a Factory to get instances of NetworkConstraintsResolver
+    public interface Builder {
+        NetworkConstraintsResolver build(int networkType);
+    }
+
+    public static class BuilderImp implements Builder {
+
+        @Override
+        public NetworkConstraintsResolver build(int networkType) {
+            switch (networkType) {
+                case INTERNET:
+                    return new InternetConstraintResolver();
+                case BLUETOOTH:
+                    return new BluetoothConstraintResolver();
+                case WIFI_P2P:
+                    return new WifiP2pConstraintResolver();
+                case NSD:
+                    return new NsdConstraintResolver();
+                default:
+                    throw new IllegalArgumentException(TAG + ": Unknown attack network type");
+            }
+        }
+    }
+
+    private static class InternetConstraintResolver extends NetworkConstraintsResolver {
+        // TODO: set the NetworkConstraint's in the queue
+    }
+
+    private static class BluetoothConstraintResolver extends NetworkConstraintsResolver {
+        // TODO: set the NetworkConstraint's in the queue
+    }
+
+    private static class WifiP2pConstraintResolver extends NetworkConstraintsResolver {
+        // TODO: set the NetworkConstraint's in the queue
+    }
+
+    private static class NsdConstraintResolver extends NetworkConstraintsResolver {
+        // TODO: set the NetworkConstraint's in the queue
+    }
 }
