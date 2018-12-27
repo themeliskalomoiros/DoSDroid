b2f1b16698f06381c1dc54f6772920cc509af8da
Tomas Michael <tomasmichael995@gmail.com>
2018-12-27 14:12:00 +0200

    Created InternetConstraint class

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/constraints/InternetConstraint.java
@@ -0,0 +1,19 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.constraints;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.utils.InternetConnectivity;
+
+class InternetConstraint extends NetworkConstraint {
+
+    @Override
+    public boolean resolve(Context context) {
+        return isConnected(context);
+    }
+
+    private boolean isConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return InternetConnectivity.isConnectionEstablishedOverWifi(cm.getActiveNetworkInfo());
+    }
+}
