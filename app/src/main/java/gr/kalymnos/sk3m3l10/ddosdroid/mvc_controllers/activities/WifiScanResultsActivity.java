c9e6fe31d8b67f0564a73a21bb13e3b7dd435e36
Tomas Michael <tomasmichael995@gmail.com>
2018-12-27 19:27:00 +0200

    Create WifiScanResultsActivity

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/WifiScanResultsActivity.java
@@ -0,0 +1,12 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WifiScanResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
