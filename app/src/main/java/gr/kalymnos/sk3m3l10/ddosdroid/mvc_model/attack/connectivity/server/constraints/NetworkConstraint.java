3a3866f5169ed1fb265fc31e33c6071984f239f9
Tomas Michael <tomasmichael995@gmail.com>
2018-12-27 13:00:28 +0200

    Created NetworkConstraint class

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/constraints/NetworkConstraint.java
@@ -0,0 +1,13 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.constraints;
+
+import android.content.Context;
+
+public abstract class NetworkConstraint {
+    protected Context context;
+
+    public NetworkConstraint(Context context) {
+        this.context = context;
+    }
+
+    public abstract boolean resolve();
+}
