53abb20f8ef17f3389b4e0b3de4798c51924553f
Tomas Michael <tomasmichael995@gmail.com>
2018-12-27 15:38:30 +0200

    Created ServerStatusBroadcaster

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/status/repository/ServerStatusBroadcaster.java
@@ -0,0 +1,17 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.status.repository;
+
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.status.ServerStatus;
+
+public class ServerStatusBroadcaster {
+    public static final String ACTION_SERVER_STATUS = "action server status broadcasted";
+    public static final String EXTRA_SERVER_STATUS = "extra server status";
+
+    public static void broadcastStatus(ServerStatus status, LocalBroadcastManager manager) {
+        Intent intent = new Intent(ACTION_SERVER_STATUS);
+        intent.putExtra(EXTRA_SERVER_STATUS, status);
+        manager.sendBroadcast(intent);
+    }
+}
