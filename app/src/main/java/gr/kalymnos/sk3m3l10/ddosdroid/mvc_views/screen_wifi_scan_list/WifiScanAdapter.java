4696093514782d1a8fd4c4d346e238ec9aaf6811
Tomas Michael <tomasmichael995@gmail.com>
2018-12-27 19:22:13 +0200

    Created WifiScanAdapter

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_views/screen_wifi_scan_list/WifiScanAdapter.java
@@ -0,0 +1,70 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_wifi_scan_list;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.R;
+
+import static gr.kalymnos.sk3m3l10.ddosdroid.utils.ValidationUtils.listHasItems;
+
+public class WifiScanAdapter extends RecyclerView.Adapter<WifiScanAdapter.ScanResultHolder> {
+    private Context context;
+    private List<String> scanResults;
+    private WifiScanViewMvc.OnWifiScanItemClickListener itemClickListener;
+
+    public WifiScanAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void addScanResults(List<String> scanResults) {
+        this.scanResults = scanResults;
+    }
+
+    public void setItemClickListener(WifiScanViewMvc.OnWifiScanItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ScanResultHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.item_spinner, viewGroup, false);
+        return new ScanResultHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ScanResultHolder viewHolder, int i) {
+        if (listHasItems(scanResults)) {
+            viewHolder.bind(scanResults.get(i));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (listHasItems(scanResults)) {
+            return scanResults.size();
+        }
+        return 0;
+    }
+
+    class ScanResultHolder extends RecyclerView.ViewHolder {
+        public ScanResultHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView.setOnClickListener((view) -> {
+                if (itemClickListener != null)
+                    itemClickListener.onWifiScanItemClick(getAdapterPosition());
+            });
+        }
+
+        void bind(String result) {
+            TextView textView = (TextView) itemView;
+            textView.setText(result);
+        }
+    }
+}
