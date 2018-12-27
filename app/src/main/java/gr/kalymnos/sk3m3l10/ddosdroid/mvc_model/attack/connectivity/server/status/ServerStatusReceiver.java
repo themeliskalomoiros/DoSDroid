542a9b8ba52130bab45a79b36daf98ea2399e357
Tomas Michael <tomasmichael995@gmail.com>
2018-12-27 15:51:45 +0200

    ServerStatusReceiver is declared abstract and handleServerStatusAction() when it receives ACTION_SERVER_STATUS

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/status/ServerStatusReceiver.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/status/ServerStatusReceiver.java
@@ -4,9 +4,21 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class ServerStatusReceiver extends BroadcastReceiver {
+import static gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.status.ServerStatusBroadcaster.ACTION_SERVER_STATUS;
+
+public abstract class ServerStatusReceiver extends BroadcastReceiver {
+    private static final String TAG = "ServerStatusReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        switch (intent.getAction()) {
+            case ACTION_SERVER_STATUS:
+                handleServerStatusAction(intent);
+                break;
+            default:
+                throw new IllegalArgumentException(TAG + ": intent does not have " + ACTION_SERVER_STATUS);
+        }
     }
+
+    protected abstract void handleServerStatusAction(Intent intent);
 }
