540389b6450fc8003b13bf6113f3ddc5414f180d
Tomas Michael <tomasmichael995@gmail.com>
2018-12-29 18:02:15 +0200

    Splitted the ServerStatusBroadcaster.broadcastStatus() into broadcastRunning()/Stopped()/Error()

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/status/ServerStatusBroadcaster.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/status/ServerStatusBroadcaster.java
@@ -1,16 +1,37 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.status;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.content.LocalBroadcastManager;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.Server;
 
+import static gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.Server.Status.ERROR;
+import static gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.Server.Status.RUNNING;
+import static gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.Server.Status.STOPPED;
+
 public class ServerStatusBroadcaster {
 
-    public static void broadcastStatus(int status, String serverId, LocalBroadcastManager manager) {
+    public static void broadcastRunning(String serverId, LocalBroadcastManager manager) {
+        Intent intent = createIntent(RUNNING, serverId);
+        manager.sendBroadcast(intent);
+    }
+
+    public static void broadcastStopped(String serverId, LocalBroadcastManager manager) {
+        Intent intent = createIntent(STOPPED, serverId);
+        manager.sendBroadcast(intent);
+    }
+
+    public static void broadcastError(String serverId, LocalBroadcastManager manager) {
+        Intent intent = createIntent(ERROR, serverId);
+        manager.sendBroadcast(intent);
+    }
+
+    @NonNull
+    private static Intent createIntent(int status, String serverId) {
         Intent intent = new Intent(Server.ACTION_SERVER_STATUS);
         intent.putExtra(Server.EXTRA_SERVER_STATUS, status);
         intent.putExtra(Server.EXTRA_ID, serverId);
-        manager.sendBroadcast(intent);
+        return intent;
     }
 }
