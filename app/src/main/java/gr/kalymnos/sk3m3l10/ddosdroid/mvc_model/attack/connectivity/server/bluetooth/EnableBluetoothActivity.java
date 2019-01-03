54a13813aeeaa23dc3e493af601553c0ae70476a
Tomas Michael <tomasmichael995@gmail.com>
2019-01-03 18:47:06 +0200

    EnableBluetoothActivity requests from user to enable Bluetooth and reports the result back to BluetoothEnableConstraint

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/bluetooth/EnableBluetoothActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/bluetooth/EnableBluetoothActivity.java
@@ -1,5 +1,9 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.bluetooth;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,5 +16,37 @@ public class EnableBluetoothActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enable_bluetooth);
+        requestBluetoothEnable();
+    }
+
+    private void requestBluetoothEnable() {
+        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(intent,REQUEST_CODE_ENABLE_BLUETOOTH);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE_ENABLE_BLUETOOTH){
+            if (resultCode == RESULT_OK){
+                sendEnableBroadcast();
+            }
+            if (resultCode == RESULT_CANCELED){
+                sendDisableBroadcast();
+            }
+        }
+    }
+
+    private void sendEnableBroadcast() {
+        Intent intent = new Intent(BluetoothEnableConstraint.ACTION_BLUETOOTH_ENABLED);
+        getLocalBroadcastManager().sendBroadcast(intent);
+    }
+
+    private void sendDisableBroadcast() {
+        Intent intent = new Intent(BluetoothEnableConstraint.ACTION_BLUETOOTH_DISABLED);
+        getLocalBroadcastManager().sendBroadcast(intent);
+    }
+
+    private LocalBroadcastManager getLocalBroadcastManager(){
+        return LocalBroadcastManager.getInstance(this);
     }
 }
