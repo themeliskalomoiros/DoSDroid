69b71aa30b0e63b24f110a42d224ffe727930871
Tomas Michael <tomasmichael995@gmail.com>
2019-01-03 17:01:13 +0200

    Created BluetoothSetupConstraint class for Bluetooth Server

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/bluetooth/BluetoothSetupConstraint.java
@@ -0,0 +1,41 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.network_constraints.NetworkConstraint;
+
+public class BluetoothSetupConstraint extends NetworkConstraint {
+    private BluetoothAdapter bluetoothAdapter;
+
+    public BluetoothSetupConstraint(Context context) {
+        super(context);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    @Override
+    public void resolve() {
+        if (isResolved()){
+            callback.onConstraintResolved(context,this);
+        }else{
+            callback.onConstraintResolveFailed(context,this);
+        }
+    }
+
+    @Override
+    public boolean isResolved() {
+        return isBluetoothSupported();
+    }
+
+    private boolean isBluetoothSupported() {
+        if (bluetoothAdapter==null){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void cleanResources() {
+
+    }
+}
