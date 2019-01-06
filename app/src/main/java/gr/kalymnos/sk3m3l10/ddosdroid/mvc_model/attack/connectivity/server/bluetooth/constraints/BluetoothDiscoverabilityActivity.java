9f86622796e2924ea04e652855453e5b9271d706
Tomas Michael <tomasmichael995@gmail.com>
2019-01-06 16:20:19 +0200

    BluetoothDiscoverabilityActivity requests discoverability from user in onCreate()

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/bluetooth/constraints/BluetoothDiscoverabilityActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/bluetooth/constraints/BluetoothDiscoverabilityActivity.java
@@ -1,12 +1,29 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.bluetooth.constraints;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BluetoothDiscoverabilityActivity extends AppCompatActivity {
+    private static final int DISCOVERABILITY_REQUEST_CODE = 1313;
+    private static final int ALWAYS_DISCOVERABLE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestDiscoverability();
+    }
+
+    private void requestDiscoverability() {
+        startActivityForResult(getDiscoverabilityIntent(),DISCOVERABILITY_REQUEST_CODE);
+    }
+
+    @NonNull
+    private Intent getDiscoverabilityIntent() {
+        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, ALWAYS_DISCOVERABLE);
+        return intent;
     }
 }
