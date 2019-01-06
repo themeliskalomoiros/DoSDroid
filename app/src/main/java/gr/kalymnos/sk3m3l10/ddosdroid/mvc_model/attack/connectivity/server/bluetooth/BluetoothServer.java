47813a7d57d16e8b9e1e47a35603474f4429ec4a
Tomas Michael <tomasmichael995@gmail.com>
2019-01-06 15:25:15 +0200

    BluetoothServer registers the BluetoothStateReceiver in constructor

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/bluetooth/BluetoothServer.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/bluetooth/BluetoothServer.java
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
@@ -12,27 +13,38 @@ import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.Serve
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.status.ServerStatusBroadcaster;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
 
+import static android.bluetooth.BluetoothAdapter.ACTION_STATE_CHANGED;
+import static android.bluetooth.BluetoothAdapter.EXTRA_STATE;
+import static android.bluetooth.BluetoothAdapter.STATE_OFF;
+
 public class BluetoothServer extends Server {
     private BroadcastReceiver bluetoothStateReceiver;
 
     public BluetoothServer(Context context, Attack attack) {
         super(context, attack);
         initializeBluetoothReceiver();
+        registerBluetoothStateReceiver(context);
     }
 
     private void initializeBluetoothReceiver() {
         bluetoothStateReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                boolean stateChanged = intent.getAction().equals(BluetoothAdapter.ACTION_STATE_CHANGED);
+                boolean stateChanged = intent.getAction().equals(ACTION_STATE_CHANGED);
                 if (stateChanged){
-                    int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,BluetoothAdapter.STATE_OFF);
-                    if (state == BluetoothAdapter.STATE_OFF){
+                    int state = intent.getIntExtra(EXTRA_STATE,STATE_OFF);
+                    if (state == STATE_OFF){
                         ServersHost.Action.stopServer(context,getId());
                     }
                 }
             }
         };
+    }
+
+    private void registerBluetoothStateReceiver(Context context) {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_STATE_CHANGED);
+        context.registerReceiver(bluetoothStateReceiver,filter);
     }
 
     @Override
