22943f246636126d8e9103fc2a731f3f25765711
Tomas Michael <tomasmichael995@gmail.com>
2019-01-06 16:05:24 +0200

    Created BluetoothDiscoverabilityConstraint class

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/bluetooth/constraints/BluetoothDiscoverabilityConstraint.java
@@ -0,0 +1,27 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.bluetooth.constraints;
+
+import android.content.Context;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.network_constraints.NetworkConstraint;
+
+public class BluetoothDiscoverabilityConstraint extends NetworkConstraint {
+
+    public BluetoothDiscoverabilityConstraint(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void resolve() {
+
+    }
+
+    @Override
+    public boolean isResolved() {
+        return false;
+    }
+
+    @Override
+    public void cleanResources() {
+
+    }
+}
