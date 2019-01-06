a02682a7774c351ccc453092c3162915a316493d
Tomas Michael <tomasmichael995@gmail.com>
2019-01-06 16:11:40 +0200

    Created BluetoothDiscoverabilityActivity class

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/bluetooth/constraints/BluetoothDiscoverabilityActivity.java
@@ -0,0 +1,12 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.bluetooth.constraints;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BluetoothDiscoverabilityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
