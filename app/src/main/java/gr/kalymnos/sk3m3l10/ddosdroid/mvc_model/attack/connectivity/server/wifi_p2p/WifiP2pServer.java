4458695818a066bafa6791e7db7a67f032cd0ab6
Tomas Michael <tomasmichael995@gmail.com>
2019-01-07 19:34:30 +0200

    Added wifi p2p permissions

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/wifi_p2p/WifiP2pServer.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/wifi_p2p/WifiP2pServer.java
@@ -7,7 +7,7 @@ import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.Serve
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
 
 public class WifiP2pServer extends Server {
-
+    //  TODO: must implement a receiver that listens when the wifi is disabled
     public WifiP2pServer(Context context, Attack attack) {
         super(context, attack);
     }
