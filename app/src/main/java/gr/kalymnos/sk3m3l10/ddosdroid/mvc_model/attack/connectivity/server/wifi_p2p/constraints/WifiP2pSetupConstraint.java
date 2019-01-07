66096e19bff6039b1601d3767c3125231ab144f8
Tomas Michael <tomasmichael995@gmail.com>
2019-01-07 20:08:08 +0200

    Created WifiP2pSetupConstraint and initialized a wifi state receiver inside its constructor

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/wifi_p2p/constraints/WifiP2pSetupConstraint.java
@@ -0,0 +1,61 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.wifi_p2p.constraints;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.network_constraints.NetworkConstraint;
+
+import static android.net.wifi.p2p.WifiP2pManager.EXTRA_WIFI_STATE;
+import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION;
+import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_STATE_ENABLED;
+
+public class WifiP2pSetupConstraint extends NetworkConstraint {
+    private static final String TAG = "WifiP2pSetupConstraint";
+
+    private BroadcastReceiver wifiStateReceiver;
+
+    public WifiP2pSetupConstraint(Context context) {
+        super(context);
+        initializeWifiStateReceiver();
+    }
+
+    private void initializeWifiStateReceiver() {
+        wifiStateReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (intent.getAction()) {
+                    case WIFI_P2P_STATE_CHANGED_ACTION:
+                        int state = intent.getIntExtra(EXTRA_WIFI_STATE, -1);
+                        handleWifiState(context, state);
+                        break;
+                    default:
+                        throw new IllegalArgumentException(TAG + ": unknown action");
+                }
+            }
+
+            private void handleWifiState(Context context, int state) {
+                if (state == WIFI_P2P_STATE_ENABLED) {
+                    callback.onConstraintResolved(context, WifiP2pSetupConstraint.this);
+                } else {
+                    callback.onConstraintResolveFailed(context, WifiP2pSetupConstraint.this);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void resolve() {
+
+    }
+
+    @Override
+    public boolean isResolved() {
+        return false;
+    }
+
+    @Override
+    public void cleanResources() {
+
+    }
+}
