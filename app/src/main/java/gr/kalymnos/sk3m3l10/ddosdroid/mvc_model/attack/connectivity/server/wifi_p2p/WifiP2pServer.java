2e50506947b1f3445fdc789efea233865c395964
Tomas Michael <tomasmichael995@gmail.com>
2019-01-08 14:14:07 +0200

    WifiP2pServer has wifiP2pManager, channel and wifiDirectReceiver fields

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/wifi_p2p/WifiP2pServer.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/wifi_p2p/WifiP2pServer.java
@@ -1,6 +1,8 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.wifi_p2p;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
@@ -10,6 +12,10 @@ import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
 
 public class WifiP2pServer extends Server {
     //  TODO: must implement a receiver that listens when the wifi is disabled
+    private WifiP2pManager wifiP2pManager;
+    private WifiP2pManager.Channel channel;
+    private BroadcastReceiver wifiDirectReceiver;
+
     public WifiP2pServer(Context context, Attack attack) {
         super(context, attack);
     }
