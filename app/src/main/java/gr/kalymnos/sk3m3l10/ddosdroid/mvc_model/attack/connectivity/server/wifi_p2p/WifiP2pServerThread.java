4b26ed88df8ebd3901d22a7c57a49ba97825beb0
Tomas Michael <tomasmichael995@gmail.com>
2019-01-23 23:34:41 +0200

    Created WifiP2pServerThread class

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/wifi_p2p/WifiP2pServerThread.java
@@ -0,0 +1,47 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.wifi_p2p;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
+
+class WifiP2pServerThread implements Runnable {
+    private static final String TAG = "WifiP2pServerThread";
+
+    private Socket socket;
+    private PrintWriter out;
+
+    public WifiP2pServerThread(@NonNull Socket socket) {
+        this.socket = socket;
+        out = new PrintWriter(getOutputStreamFrom(socket), true);
+    }
+
+    private OutputStream getOutputStreamFrom(@NonNull Socket socket) {
+        OutputStream outputStream = null;
+        try {
+            outputStream = socket.getOutputStream();
+        } catch (IOException e) {
+            Log.e(TAG, "Error obtaining outStream from socket", e);
+        }
+        return outputStream;
+    }
+
+    @Override
+    public void run() {
+        out.println(Attack.STARTED_PASS);
+        closeSocket();
+    }
+
+    private void closeSocket() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
