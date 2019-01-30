54f067151bc063e8c8a3ea043a5454b7eca7ace3
Tomas Michael <tomasmichael995@gmail.com>
2019-01-30 14:47:14 +0200

    BluetoothClientConnection initializes a discoveryTask thread

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/BluetoothClientConnection.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/BluetoothClientConnection.java
@@ -1,26 +1,39 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.client;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.NetworkConstraintsResolver;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants;
-import gr.kalymnos.sk3m3l10.ddosdroid.utils.BluetoothUtils;
 
 import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.Extra.EXTRA_MAC_ADDRESS;
 import static gr.kalymnos.sk3m3l10.ddosdroid.utils.BluetoothUtils.isThisDevicePairedWith;
 
 class BluetoothClientConnection extends ClientConnection implements NetworkConstraintsResolver.OnConstraintsResolveListener {
     private NetworkConstraintsResolver constraintsResolver;
+    private Thread discoveryTask;
 
     BluetoothClientConnection(Context context, Attack attack) {
         super(context, attack);
         initializeResolver(context);
+        initializeDiscoveryTask();
     }
 
     private void initializeResolver(Context context) {
         NetworkConstraintsResolver.Builder builder = new NetworkConstraintsResolver.BuilderImp();
         constraintsResolver = builder.build(context, Constants.NetworkType.BLUETOOTH);
+    }
+
+    private void initializeDiscoveryTask() {
+        discoveryTask = new Thread(() -> {
+            BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+            boolean discoveryInitiated = adapter.startDiscovery();
+            if (!discoveryInitiated) {
+                connectionListener.onConnectionError();
+                disconnect();
+            }
+        });
     }
 
     @Override
