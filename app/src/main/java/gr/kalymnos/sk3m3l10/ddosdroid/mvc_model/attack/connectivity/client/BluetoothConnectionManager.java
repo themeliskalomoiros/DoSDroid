aff391a4cd86de225ae3269462866d2fc43280f3
Tomas Michael <tomasmichael995@gmail.com>
2019-01-30 17:38:37 +0200

    Added logs to BluetoothConnectionManager

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/BluetoothConnectionManager.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/BluetoothConnectionManager.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.util.Log;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.NetworkConstraintsResolver;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
@@ -15,6 +16,8 @@ import gr.kalymnos.sk3m3l10.ddosdroid.utils.BluetoothDeviceUtils;
 
 class BluetoothConnectionManager extends ConnectionManager implements NetworkConstraintsResolver.OnConstraintsResolveListener,
         BluetoothConnectionThread.OnBluetoothConnectionListener {
+    private static final String TAG = "BluetoothConnectionMana";
+
     private NetworkConstraintsResolver constraintsResolver;
     private Thread discoveryTask;
     private BroadcastReceiver deviceDiscoveryReceiver;
@@ -44,6 +47,9 @@ class BluetoothConnectionManager extends ConnectionManager implements NetworkCon
             if (!discoveryInitiated) {
                 connectionListener.onConnectionError();
                 disconnect();
+                Log.d(TAG, "Device discovery failed to initiate");
+            } else {
+                Log.d(TAG, "Device discovery initiated");
             }
         });
     }
@@ -52,10 +58,13 @@ class BluetoothConnectionManager extends ConnectionManager implements NetworkCon
         deviceDiscoveryReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
+                Log.d(TAG, "deviceDiscoveryReceiver.OnReceiver() called");
                 String action = intent.getAction();
                 if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    Log.d(TAG, "A device discovered");
                     BluetoothDevice discoveredDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                     if (isServerDeviceDiscovered(discoveredDevice)) {
+                        Log.d(TAG, "Server device discovered, proceeding to connection");
                         context.unregisterReceiver(this);
                         BluetoothConnectionThread.startAnInstance(discoveredDevice, Attacks.getUUID(attack), BluetoothConnectionManager.this);
                     }
@@ -74,6 +83,7 @@ class BluetoothConnectionManager extends ConnectionManager implements NetworkCon
         IntentFilter filter = new IntentFilter();
         filter.addAction(BluetoothDevice.ACTION_FOUND);
         context.registerReceiver(deviceDiscoveryReceiver, filter);
+        Log.d(TAG, "Receiver registered");
     }
 
     @Override
@@ -95,11 +105,14 @@ class BluetoothConnectionManager extends ConnectionManager implements NetworkCon
 
     @Override
     public void onConstraintsResolved() {
+        Log.d(TAG, "BluetoothConnectionManager constraints resolved");
         String serverMacAddress = Attacks.getMacAddress(attack);
         if (BluetoothDeviceUtils.isThisDevicePairedWith(serverMacAddress)) {
+            Log.d(TAG, "Server device is already paired with device, proceeding to connection");
             BluetoothConnectionThread.startAnInstance(BluetoothDeviceUtils.getPairedBluetoothDeviceOf(serverMacAddress),
                     Attacks.getUUID(attack), this);
         } else {
+            Log.d(TAG, "Server device was not paired with local device, proceeding to discovery");
             discoveryTask.start();
         }
     }
