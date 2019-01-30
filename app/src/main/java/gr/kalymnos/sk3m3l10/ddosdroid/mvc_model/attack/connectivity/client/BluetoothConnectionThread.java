c4d399170c89b4ef46618f7846bc647c1172e153
Tomas Michael <tomasmichael995@gmail.com>
2019-01-30 15:36:24 +0200

    BluetoothConnectionThread initializes the BluetoothSocket through BLuetoothDevice parameter inside constructor

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/BluetoothConnectionThread.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/BluetoothConnectionThread.java
@@ -1,9 +1,11 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.client;
 
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.util.Log;
 
 import java.io.IOException;
+import java.util.UUID;
 
 class BluetoothConnectionThread extends Thread {
     private static final String TAG = "BluetoothConnectionThre";
@@ -17,8 +19,16 @@ class BluetoothConnectionThread extends Thread {
         void onBluetoothConnectionFailure();
     }
 
-    public BluetoothConnectionThread(BluetoothSocket bluetoothSocket) {
-        this.bluetoothSocket = bluetoothSocket;
+    public BluetoothConnectionThread(BluetoothDevice device, UUID serverUUID) {
+        initializeBluetoothSocket(device, serverUUID);
+    }
+
+    private void initializeBluetoothSocket(BluetoothDevice device, UUID serverUUID) {
+        try {
+            this.bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(serverUUID);
+        } catch (IOException e) {
+            Log.e(TAG, "Error when initializing bluetoothSocket", e);
+        }
     }
 
     public void setOnBluetoothConnectionListener(OnBluetoothConnectionListener onBluetoothConnectionListener) {
@@ -39,7 +49,7 @@ class BluetoothConnectionThread extends Thread {
             bluetoothSocket.connect();
             return true;
         } catch (IOException e) {
-            Log.d(TAG, "Error when bluetoothSocket.connect()", e);
+            Log.e(TAG, "Error when bluetoothSocket.connect()", e);
             return false;
         }
     }
