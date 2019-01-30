7ea823ae7ce5e529e4e8c506e0d56772533080be
Tomas Michael <tomasmichael995@gmail.com>
2019-01-30 21:15:07 +0200

    Removed AttackRepository field from JoinAttackActivity, never used

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/JoinAttackActivity.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_controllers/activities/JoinAttackActivity.java
@@ -5,35 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.fragments.JoinAttackInfoFragment;
-import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.repository.AttackRepository;
-import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.repository.FirebaseRepository;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.service.AttackService;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack.JoinAttackViewMvc;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_views.screen_join_attack.JoinAttackViewMvcImp;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
-import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attacks;
-import gr.kalymnos.sk3m3l10.ddosdroid.pojos.bot.Bots;
-
-import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.Extra.EXTRA_ATTACK;
 
 public class JoinAttackActivity extends AppCompatActivity implements
         JoinAttackInfoFragment.OnJoinAttackButtonClickListener {
     private JoinAttackViewMvc viewMvc;
-    private AttackRepository repo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initializeFields();
         setupUi();
     }
 
-    private void initializeFields() {
-        repo = new FirebaseRepository();
-        viewMvc = new JoinAttackViewMvcImp(LayoutInflater.from(this), null);
-    }
-
     private void setupUi() {
+        viewMvc = new JoinAttackViewMvcImp(LayoutInflater.from(this), null);
         setSupportActionBar(viewMvc.getToolbar());
         setContentView(viewMvc.getRootView());
         showJoinAttackInfoFragment();
