6a0d586f91b8690b57e291d1d22b3a8cbbbe52f4
Tomas Michael <tomasmichael995@gmail.com>
2019-01-30 12:27:29 +0200

    Created one more build() for NetworkConstraintsResolver.Builder

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/network_constraints/NetworkConstraintsResolver.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/network_constraints/NetworkConstraintsResolver.java
@@ -6,14 +6,14 @@ import android.support.annotation.NonNull;
 import java.util.LinkedList;
 import java.util.Queue;
 
-import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.Server;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.bluetooth.BluetoothDiscoverabilityConstraint;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.bluetooth.BluetoothEnableConstraint;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.bluetooth.BluetoothSetupConstraint;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.internet.InternetConstraint;
-import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.wifi_p2p.WifiP2pServer;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.wifi_p2p.WifiP2pEnabledConstraint;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.wifi_p2p.WifiP2pGroupConstraint;
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.Server;
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.server.wifi_p2p.WifiP2pServer;
 
 import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.BLUETOOTH;
 import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.NetworkType.INTERNET;
@@ -84,6 +84,8 @@ public class NetworkConstraintsResolver implements NetworkConstraint.OnResolveCo
 
     public interface Builder {
         NetworkConstraintsResolver build(Context context, int networkType, Server server);
+
+        NetworkConstraintsResolver build(Context context, int networkType);
     }
 
     public static class BuilderImp implements Builder {
@@ -103,70 +105,104 @@ public class NetworkConstraintsResolver implements NetworkConstraint.OnResolveCo
                     throw new IllegalArgumentException(TAG + ": Unknown attack network type");
             }
         }
-    }
 
-    private static class InternetServerResolver extends NetworkConstraintsResolver {
-        public InternetServerResolver(Context context) {
-            addConstraint(createInternetConstraint(context));
+        private static class InternetServerResolver extends NetworkConstraintsResolver {
+            public InternetServerResolver(Context context) {
+                addConstraint(createInternetConstraint(context));
+            }
+
+            @NonNull
+            private InternetConstraint createInternetConstraint(Context context) {
+                InternetConstraint constraint = new InternetConstraint(context);
+                constraint.setOnResolveConstraintListener(this);
+                return constraint;
+            }
         }
 
-        @NonNull
-        private InternetConstraint createInternetConstraint(Context context) {
-            InternetConstraint constraint = new InternetConstraint(context);
-            constraint.setOnResolveConstraintListener(this);
-            return constraint;
-        }
-    }
+        private static class BluetoothServerResolver extends NetworkConstraintsResolver {
+            public BluetoothServerResolver(Context context) {
+                addConstraint(createSetupConstraint(context));
+                addConstraint(createBluetoothEnableConstraint(context));
+                addConstraint(createBluetoothDiscoverabilityConstraint(context));
+            }
 
-    private static class BluetoothServerResolver extends NetworkConstraintsResolver {
-        public BluetoothServerResolver(Context context) {
-            addConstraint(createSetupConstraint(context));
-            addConstraint(createBluetoothEnableConstraint(context));
-            addConstraint(createBluetoothDiscoverabilityConstraint(context));
-        }
+            private BluetoothSetupConstraint createSetupConstraint(Context context) {
+                BluetoothSetupConstraint constraint = new BluetoothSetupConstraint(context);
+                constraint.setOnResolveConstraintListener(this);
+                return constraint;
+            }
 
-        private BluetoothSetupConstraint createSetupConstraint(Context context) {
-            BluetoothSetupConstraint constraint = new BluetoothSetupConstraint(context);
-            constraint.setOnResolveConstraintListener(this);
-            return constraint;
-        }
+            private BluetoothEnableConstraint createBluetoothEnableConstraint(Context context) {
+                BluetoothEnableConstraint constraint = new BluetoothEnableConstraint(context);
+                constraint.setOnResolveConstraintListener(this);
+                return constraint;
+            }
 
-        private BluetoothEnableConstraint createBluetoothEnableConstraint(Context context) {
-            BluetoothEnableConstraint constraint = new BluetoothEnableConstraint(context);
-            constraint.setOnResolveConstraintListener(this);
-            return constraint;
-        }
-
-        @NonNull
-        private BluetoothDiscoverabilityConstraint createBluetoothDiscoverabilityConstraint(Context context) {
-            BluetoothDiscoverabilityConstraint constraint = new BluetoothDiscoverabilityConstraint(context);
-            constraint.setOnResolveConstraintListener(this);
-            return constraint;
-        }
-    }
-
-    private static class WifiP2pServerResolver extends NetworkConstraintsResolver {
-        public WifiP2pServerResolver(Context context, Server server) {
-            addConstraint(createWifiP2pSetupConstraint(context));
-            addConstraint(createWifiP2pGroupConstraint(context, (WifiP2pServer) server));
+            @NonNull
+            private BluetoothDiscoverabilityConstraint createBluetoothDiscoverabilityConstraint(Context context) {
+                BluetoothDiscoverabilityConstraint constraint = new BluetoothDiscoverabilityConstraint(context);
+                constraint.setOnResolveConstraintListener(this);
+                return constraint;
+            }
         }
 
-        @NonNull
-        private WifiP2pGroupConstraint createWifiP2pGroupConstraint(Context context, WifiP2pServer server) {
-            WifiP2pGroupConstraint constraint = new WifiP2pGroupConstraint(context, server);
-            constraint.setOnResolveConstraintListener(this);
-            return constraint;
+        private static class WifiP2pServerResolver extends NetworkConstraintsResolver {
+            public WifiP2pServerResolver(Context context, Server server) {
+                addConstraint(createWifiP2pSetupConstraint(context));
+                addConstraint(createWifiP2pGroupConstraint(context, (WifiP2pServer) server));
+            }
+
+            @NonNull
+            private WifiP2pGroupConstraint createWifiP2pGroupConstraint(Context context, WifiP2pServer server) {
+                WifiP2pGroupConstraint constraint = new WifiP2pGroupConstraint(context, server);
+                constraint.setOnResolveConstraintListener(this);
+                return constraint;
+            }
+
+            @NonNull
+            private WifiP2pEnabledConstraint createWifiP2pSetupConstraint(Context context) {
+                WifiP2pEnabledConstraint constraint = new WifiP2pEnabledConstraint(context);
+                constraint.setOnResolveConstraintListener(this);
+                return constraint;
+            }
         }
 
-        @NonNull
-        private WifiP2pEnabledConstraint createWifiP2pSetupConstraint(Context context) {
-            WifiP2pEnabledConstraint constraint = new WifiP2pEnabledConstraint(context);
-            constraint.setOnResolveConstraintListener(this);
-            return constraint;
+        private static class NsdServerResolver extends NetworkConstraintsResolver {
+            // TODO: set the NetworkConstraint's in the queue
         }
-    }
 
-    private static class NsdServerResolver extends NetworkConstraintsResolver {
-        // TODO: set the NetworkConstraint's in the queue
+        @Override
+        public NetworkConstraintsResolver build(Context context, int networkType) {
+            switch (networkType) {
+                case INTERNET:
+                    return new InternetClientResolver(context);
+                case BLUETOOTH:
+                    return new BluetoothClientResolver(context);
+                case WIFI_P2P:
+                    return new WifiP2pClientResolver(context);
+                case NSD:
+                    return new NsdClientResolver();
+                default:
+                    throw new IllegalArgumentException(TAG + ": Unknown attack network type");
+            }
+        }
+
+        private class InternetClientResolver extends NetworkConstraintsResolver {
+            public InternetClientResolver(Context context) {
+            }
+        }
+
+        private class BluetoothClientResolver extends NetworkConstraintsResolver {
+            public BluetoothClientResolver(Context context) {
+            }
+        }
+
+        private class WifiP2pClientResolver extends NetworkConstraintsResolver {
+            public WifiP2pClientResolver(Context context) {
+            }
+        }
+
+        private class NsdClientResolver extends NetworkConstraintsResolver {
+        }
     }
 }
