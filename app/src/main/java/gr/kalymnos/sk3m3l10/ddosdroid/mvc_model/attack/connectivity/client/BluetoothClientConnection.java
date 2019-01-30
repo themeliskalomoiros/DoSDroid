bc6eb2139d3c065ee464c845d7f283fa7f94cdc6
Tomas Michael <tomasmichael995@gmail.com>
2019-01-30 12:43:36 +0200

    BluetoothClientConnection has a NetworkConstraintsResolver field and implements NetworkConstraintsResolver.OnConstraintsResolveListener

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/BluetoothClientConnection.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/BluetoothClientConnection.java
@@ -2,12 +2,21 @@ package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.client;
 
 import android.content.Context;
 
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.NetworkConstraintsResolver;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
+import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants;
 
-class BluetoothClientConnection extends ClientConnection {
+class BluetoothClientConnection extends ClientConnection implements NetworkConstraintsResolver.OnConstraintsResolveListener {
+    private NetworkConstraintsResolver constraintsResolver;
 
     BluetoothClientConnection(Context context, Attack attack) {
         super(context, attack);
+        initializeResolver(context);
+    }
+
+    private void initializeResolver(Context context) {
+        NetworkConstraintsResolver.Builder builder = new NetworkConstraintsResolver.BuilderImp();
+        constraintsResolver = builder.build(context, Constants.NetworkType.BLUETOOTH);
     }
 
     @Override
@@ -23,5 +32,15 @@ class BluetoothClientConnection extends ClientConnection {
     @Override
     protected void releaseResources() {
         super.releaseResources();
+    }
+
+    @Override
+    public void onConstraintsResolved() {
+
+    }
+
+    @Override
+    public void onConstraintResolveFailure() {
+
     }
 }
