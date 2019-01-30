09430b3a42db995fc5b2c450d28e3234f1e23ad7
Tomas Michael <tomasmichael995@gmail.com>
2019-01-30 16:39:25 +0200

    Created BluetoothDeviceUtils.getPairedBluetoothDeviceOf()

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/BluetoothDeviceUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/BluetoothDeviceUtils.java
@@ -6,6 +6,7 @@ import android.bluetooth.BluetoothDevice;
 import java.util.Set;
 
 public class BluetoothDeviceUtils {
+    private static final String TAG = "BluetoothDeviceUtils";
 
     public static boolean isThisDevicePairedWith(String deviceAddress /*MAC Address*/) {
         for (BluetoothDevice device : getPairedDevices()) {
@@ -16,7 +17,16 @@ public class BluetoothDeviceUtils {
         return false;
     }
 
-    public static Set<BluetoothDevice> getPairedDevices() {
+    public static BluetoothDevice getPairedBluetoothDeviceOf(String deviceAddress) {
+        for (BluetoothDevice device : getPairedDevices()) {
+            if (device.getAddress().equals(deviceAddress)) {
+                return device;
+            }
+        }
+        throw new UnsupportedOperationException(TAG + ": Cound not find bluetoothDevice");
+    }
+
+    private static Set<BluetoothDevice> getPairedDevices() {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         return bluetoothAdapter.getBondedDevices();
     }
