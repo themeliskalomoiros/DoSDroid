9e623389be98b44efff3930f6d1430f2264e9081
Tomas Michael <tomasmichael995@gmail.com>
2019-02-03 13:24:16 +0200

    Created WifiP2PConnectionManager class

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/WifiP2PConnectionManager.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/WifiP2PConnectionManager.java
@@ -2,12 +2,21 @@ package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.client;
 
 import android.content.Context;
 
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.NetworkConstraintsResolver;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
 
-class WifiP2PConnectionManager extends ConnectionManager {
+class WifiP2PConnectionManager extends ConnectionManager implements NetworkConstraintsResolver.OnConstraintsResolveListener {
+    private NetworkConstraintsResolver constraintsResolver;
 
     WifiP2PConnectionManager(Context context, Attack attack) {
         super(context, attack);
+        initializeConstraintsResolver(context, attack);
+    }
+
+    private void initializeConstraintsResolver(Context context, Attack attack) {
+        NetworkConstraintsResolver.Builder builder = new NetworkConstraintsResolver.BuilderImp();
+        constraintsResolver = builder.build(context, attack.getNetworkType());
+        constraintsResolver.setOnConstraintsResolveListener(this);
     }
 
     @Override
@@ -23,5 +32,15 @@ class WifiP2PConnectionManager extends ConnectionManager {
     @Override
     protected void releaseResources() {
         super.releaseResources();
+    }
+
+    @Override
+    public void onConstraintsResolved() {
+
+    }
+
+    @Override
+    public void onConstraintResolveFailure() {
+
     }
 }
