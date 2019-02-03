36371b35618da9316bf01562ff15b35fef4cc7bf
Tomas Michael <tomasmichael995@gmail.com>
2019-02-03 13:57:24 +0200

    WifiP2PConnectionManager initializes a WifiP2pManager inside its constructor

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/WifiP2PConnectionManager.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/WifiP2PConnectionManager.java
@@ -1,16 +1,24 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.client;
 
 import android.content.Context;
+import android.net.wifi.p2p.WifiP2pManager;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.NetworkConstraintsResolver;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
 
 class WifiP2PConnectionManager extends ConnectionManager implements NetworkConstraintsResolver.OnConstraintsResolveListener {
     private NetworkConstraintsResolver constraintsResolver;
+    private WifiP2pManager.Channel channel;
+    private WifiP2pManager wifiP2pManager;
 
     WifiP2PConnectionManager(Context context, Attack attack) {
         super(context, attack);
+        initializeFields(context, attack);
+    }
+
+    private void initializeFields(Context context, Attack attack) {
         initializeConstraintsResolver(context, attack);
+        wifiP2pManager = (WifiP2pManager) context.getSystemService(Context.WIFI_P2P_SERVICE);
     }
 
     private void initializeConstraintsResolver(Context context, Attack attack) {
