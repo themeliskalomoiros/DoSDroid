98e5e0b904ddf2fb6266738c14028fe4b801ed40
Tomas Michael <tomasmichael995@gmail.com>
2019-02-03 14:04:05 +0200

    WifiP2PConnectionManager initializes a wifiDirectReceiver

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/WifiP2PConnectionManager.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/WifiP2PConnectionManager.java
@@ -1,6 +1,8 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.client;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Looper;
 
@@ -8,9 +10,12 @@ import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_cons
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
 
 class WifiP2PConnectionManager extends ConnectionManager implements NetworkConstraintsResolver.OnConstraintsResolveListener {
+    private static final String TAG = "WifiP2PConnectionManage";
+
     private NetworkConstraintsResolver constraintsResolver;
     private WifiP2pManager.Channel channel;
     private WifiP2pManager wifiP2pManager;
+    private BroadcastReceiver wifiDirectReceiver;
 
     WifiP2PConnectionManager(Context context, Attack attack) {
         super(context, attack);
@@ -18,14 +23,39 @@ class WifiP2PConnectionManager extends ConnectionManager implements NetworkConst
     }
 
     private void initializeFields(Context context, Attack attack) {
-        initializeConstraintsResolver(context, attack);
         wifiP2pManager = (WifiP2pManager) context.getSystemService(Context.WIFI_P2P_SERVICE);
+        initializeConstraintsResolver(context, attack);
+        initializeWifiDirectReceiver();
     }
 
     private void initializeConstraintsResolver(Context context, Attack attack) {
         NetworkConstraintsResolver.Builder builder = new NetworkConstraintsResolver.BuilderImp();
         constraintsResolver = builder.build(context, attack.getNetworkType());
         constraintsResolver.setOnConstraintsResolveListener(this);
+    }
+
+    private void initializeWifiDirectReceiver() {
+        wifiDirectReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (intent.getAction()) {
+                    case WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION:
+                        //  Check to see if wifi is enabled/disabled
+                        break;
+                    case WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION:
+                        //  Call WifiP2pManager.requestPeers() to get a list of current peers
+                        break;
+                    case WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION:
+                        //  Respond to new connection or disconnections
+                        break;
+                    case WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION:
+                        //  Respond to this device's wifi state changing
+                        break;
+                    default:
+                        throw new IllegalArgumentException(TAG + ": Unknown action");
+                }
+            }
+        };
     }
 
     @Override
