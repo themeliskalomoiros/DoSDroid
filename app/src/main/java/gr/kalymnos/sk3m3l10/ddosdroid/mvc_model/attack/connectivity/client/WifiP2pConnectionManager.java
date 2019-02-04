f85ef49caeabeb0cc4f192431e234ca376e98f72
Tomas Michael <tomasmichael995@gmail.com>
2019-02-04 15:13:14 +0200

    Created WifiP2pConnectionManager from the begining

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/WifiP2pConnectionManager.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/WifiP2pConnectionManager.java
@@ -32,200 +32,37 @@ import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.Extra.EXTRA_
 class WifiP2pConnectionManager extends ConnectionManager implements NetworkConstraintsResolver.OnConstraintsResolveListener, WifiP2pConnectionThread.OnConnectionListener {
     private static final String TAG = "WifiP2pConnectionManage";
 
-    private NetworkConstraintsResolver constraintsResolver;
-    private WifiP2pManager.Channel channel;
-    private WifiP2pManager wifiP2pManager;
-    private BroadcastReceiver wifiDirectReceiver;
-    private WifiP2pConnectionThread connectionThread;
-    private boolean connectionToDeviceInitiated = false;
-
     WifiP2pConnectionManager(Context context, Attack attack) {
         super(context, attack);
-        initializeFields(context, attack);
-    }
-
-    private void initializeFields(Context context, Attack attack) {
-        wifiP2pManager = (WifiP2pManager) context.getSystemService(Context.WIFI_P2P_SERVICE);
-        initializeConstraintsResolver(context, attack);
-        initializeWifiDirectReceiver();
-        registerWifiDirectReceiver(context);
-    }
-
-    private void initializeConstraintsResolver(Context context, Attack attack) {
-        NetworkConstraintsResolver.Builder builder = new NetworkConstraintsResolver.BuilderImp();
-        constraintsResolver = builder.build(context, attack.getNetworkType());
-        constraintsResolver.setOnConstraintsResolveListener(this);
-    }
-
-    private void initializeWifiDirectReceiver() {
-        wifiDirectReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                switch (intent.getAction()) {
-                    case WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION:
-                        //  Check to see if wifi is enabled/disabled
-                        int state = intent.getIntExtra(EXTRA_WIFI_STATE, -1);
-                        handleWifiState(context, state);
-                        break;
-                    case WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION:
-                        //  Call WifiP2pManager.requestPeers() to get a list of current peers
-                        wifiP2pManager.requestPeers(channel, getPeerListener());
-                        break;
-                    case WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION:
-                        //  Respond to new connection or disconnections
-                        Log.d(TAG, "WIFI_P2P_CONNECTION_CHANGED_ACTION received");
-                        NetworkInfo networkInfo = intent.getParcelableExtra(EXTRA_NETWORK_INFO);
-                        if (networkInfo.isConnected()) {
-                            //  We are connected with the device, requesting connection info
-                            //  to find the group owner's IP
-                            Log.d(TAG, "NetworkInfo.isConnected() == true");
-                            wifiP2pManager.requestConnectionInfo(channel, wifiP2pInfo -> {
-                                if (wifiP2pInfo.groupFormed && !wifiP2pInfo.isGroupOwner) {
-                                    if (connectionThread != null) {
-                                        return;
-                                    }
-                                    initializeConnectionThread(wifiP2pInfo);
-                                    connectionThread.start();
-                                }
-                            });
-                        } else {
-                            Log.d(TAG, "networkInfo.isConnected() == false");
-                        }
-                        break;
-                    case WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION:
-                        //  Respond to this device's wifi state changing
-                        break;
-                    default:
-                        throw new IllegalArgumentException(TAG + ": Unknown action");
-                }
-            }
-
-            private void handleWifiState(Context context, int state) {
-                if (state != WIFI_P2P_STATE_ENABLED) {
-                    disconnect();
-                }
-            }
-
-            private WifiP2pManager.PeerListListener getPeerListener() {
-                return new WifiP2pManager.PeerListListener() {
-                    @Override
-                    public void onPeersAvailable(WifiP2pDeviceList deviceList) {
-                        for (WifiP2pDevice device : deviceList.getDeviceList()) {
-                            if (isServerDevice(device)) {
-                                connectTo(device);
-                            }
-                        }
-                    }
-
-                    private boolean isServerDevice(WifiP2pDevice device) {
-                        return device.deviceAddress.equals(attack.getHostInfo().get(EXTRA_MAC_ADDRESS))
-                                && device.isGroupOwner();
-                    }
-
-                    private void connectTo(WifiP2pDevice device) {
-                        if (!connectionToDeviceInitiated) {
-                            Log.d(TAG, "connectTo() called");
-                            WifiP2pConfig config = new WifiP2pConfig();
-                            config.deviceAddress = device.deviceAddress;
-                            config.wps.setup = WpsInfo.PBC;
-                            wifiP2pManager.connect(channel, config, getConnectActionListener());
-                        }
-                    }
-
-                    @NonNull
-                    private WifiP2pManager.ActionListener getConnectActionListener() {
-                        return new WifiP2pManager.ActionListener() {
-                            @Override
-                            public void onSuccess() {
-                                Log.d(TAG, "Connection to server device initiated.");
-                                connectionToDeviceInitiated = true;
-                            }
-
-                            @Override
-                            public void onFailure(int reason) {
-                                Log.d(TAG, "Failed to initiate connection to server device: " + WifiP2pUtils.getFailureTextFrom(reason));
-                                disconnect();
-                            }
-                        };
-                    }
-                };
-            }
-
-            private void initializeConnectionThread(WifiP2pInfo wifiP2pInfo) {
-                connectionThread = new WifiP2pConnectionThread(wifiP2pInfo.groupOwnerAddress, Attacks.getHostLocalPort(attack));
-                connectionThread.setConnectionListener(WifiP2pConnectionManager.this);
-            }
-        };
-    }
-
-    private void registerWifiDirectReceiver(Context context) {
-        context.registerReceiver(wifiDirectReceiver, getIntentFilter());
-    }
-
-    @NonNull
-    private IntentFilter getIntentFilter() {
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(WIFI_P2P_STATE_CHANGED_ACTION);
-        filter.addAction(WIFI_P2P_PEERS_CHANGED_ACTION);
-        filter.addAction(WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        filter.addAction(WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-        return filter;
     }
 
     @Override
     void connect() {
-        constraintsResolver.resolveConstraints();
+
     }
 
     @Override
     void disconnect() {
-        client.onManagerDisconnection();
-        releaseResources();
-    }
 
-    @Override
-    protected void releaseResources() {
-        constraintsResolver.releaseResources();
-        context.unregisterReceiver(wifiDirectReceiver);
-        super.releaseResources();
-    }
-
-    @Override
-    public void onConstraintsResolved() {
-        channel = wifiP2pManager.initialize(context, Looper.getMainLooper(), null);
-        wifiP2pManager.discoverPeers(channel, getDiscoveryActionListener());
-    }
-
-    @NonNull
-    private WifiP2pManager.ActionListener getDiscoveryActionListener() {
-        return new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() {
-                Log.d(TAG, "Peer discovery initiated.");
-            }
-
-            @Override
-            public void onFailure(int reason) {
-                Log.d(TAG, "Failed to initiate peer discovery: " + WifiP2pUtils.getFailureTextFrom(reason));
-                Log.d(TAG, "Disconnecting...");
-                disconnect();
-            }
-        };
-    }
-
-    @Override
-    public void onConstraintResolveFailure() {
-        client.onManagerError();
-        releaseResources();
     }
 
     @Override
     public void onConnectionSuccess() {
-        client.onManagerConnection();
+
     }
 
     @Override
     public void onConnectionFailure() {
-        client.onManagerError();
+
+    }
+
+    @Override
+    public void onConstraintsResolved() {
+
+    }
+
+    @Override
+    public void onConstraintResolveFailure() {
+
     }
 }
