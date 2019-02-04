546539894070271c67277edebe7330835b3f3fc6
Tomas Michael <tomasmichael995@gmail.com>
2019-02-04 15:17:36 +0200

    WifiP2pConnectionManager initializes a WifiDirectReceiver

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/WifiP2pConnectionManager.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/WifiP2pConnectionManager.java
@@ -1,68 +1,35 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.client;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.NetworkInfo;
-import android.net.wifi.WpsInfo;
-import android.net.wifi.p2p.WifiP2pConfig;
-import android.net.wifi.p2p.WifiP2pDevice;
-import android.net.wifi.p2p.WifiP2pDeviceList;
-import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Looper;
-import android.support.annotation.NonNull;
-import android.util.Log;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.network_constraints.NetworkConstraintsResolver;
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
-import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attacks;
-import gr.kalymnos.sk3m3l10.ddosdroid.utils.WifiP2pUtils;
 
-import static android.net.wifi.p2p.WifiP2pManager.EXTRA_NETWORK_INFO;
-import static android.net.wifi.p2p.WifiP2pManager.EXTRA_WIFI_STATE;
-import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION;
-import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION;
-import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION;
-import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_STATE_ENABLED;
-import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION;
-import static gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Constants.Extra.EXTRA_MAC_ADDRESS;
-
-class WifiP2pConnectionManager extends ConnectionManager implements NetworkConstraintsResolver.OnConstraintsResolveListener, WifiP2pConnectionThread.OnConnectionListener {
+class WifiP2pConnectionManager extends ConnectionManager{
     private static final String TAG = "WifiP2pConnectionManage";
+
+    private WifiDirectReceiver receiver;
 
     WifiP2pConnectionManager(Context context, Attack attack) {
         super(context, attack);
+        initializeReceiver(context);
+    }
+
+    private void initializeReceiver(Context context) {
+        WifiP2pManager manager = (WifiP2pManager) context.getSystemService(Context.WIFI_P2P_SERVICE);
+        WifiP2pManager.Channel channel = manager.initialize(context, Looper.getMainLooper(), null);
+        receiver = new WifiDirectReceiver(this, manager, channel);
     }
 
     @Override
     void connect() {
-
+        context.registerReceiver(receiver, WifiDirectReceiver.getIntentFilter());
     }
 
     @Override
     void disconnect() {
-
-    }
-
-    @Override
-    public void onConnectionSuccess() {
-
-    }
-
-    @Override
-    public void onConnectionFailure() {
-
-    }
-
-    @Override
-    public void onConstraintsResolved() {
-
-    }
-
-    @Override
-    public void onConstraintResolveFailure() {
 
     }
 }
