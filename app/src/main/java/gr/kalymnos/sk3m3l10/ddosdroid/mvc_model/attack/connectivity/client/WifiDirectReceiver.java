38520110b4ebb5ffb00ba152aa7a33ea3484fd19
Tomas Michael <tomasmichael995@gmail.com>
2019-02-04 15:14:14 +0200

    Created WifiDirectReceiver class

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/WifiDirectReceiver.java
@@ -0,0 +1,59 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.client;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.support.annotation.NonNull;
+
+import static android.net.wifi.p2p.WifiP2pManager.EXTRA_WIFI_STATE;
+import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION;
+import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION;
+import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION;
+import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_STATE_ENABLED;
+import static android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION;
+
+public class WifiDirectReceiver extends BroadcastReceiver {
+    private static final String TAG = "WifiDirectReceiver";
+
+    private WifiP2pConnectionManager connectionManager;
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+
+    public WifiDirectReceiver(WifiP2pConnectionManager connectionManager, WifiP2pManager manager, WifiP2pManager.Channel channel) {
+        this.connectionManager = connectionManager;
+        this.manager = manager;
+        this.channel = channel;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        switch (intent.getAction()) {
+            case WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION:
+                int state = intent.getIntExtra(EXTRA_WIFI_STATE, -1);
+                if (state != WIFI_P2P_STATE_ENABLED) {
+                    connectionManager.disconnect();
+                }
+                break;
+            case WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION:
+                break;
+            case WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION:
+                break;
+            case WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION:
+                break;
+            default:
+                throw new IllegalArgumentException(TAG + ": Unknown action");
+        }
+    }
+
+    @NonNull
+    public static IntentFilter getIntentFilter() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WIFI_P2P_STATE_CHANGED_ACTION);
+        filter.addAction(WIFI_P2P_PEERS_CHANGED_ACTION);
+        filter.addAction(WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        filter.addAction(WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        return filter;
+    }
+}
