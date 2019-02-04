92c38c86681d3ee515be18256be3e128843d2e17
Tomas Michael <tomasmichael995@gmail.com>
2019-02-04 18:40:44 +0200

    Created NsdConnectionManager

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/NsdConnectionManager.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/client/NsdConnectionManager.java
@@ -1,10 +1,14 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.attack.connectivity.client;
 
 import android.content.Context;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+import android.util.Log;
 
 import gr.kalymnos.sk3m3l10.ddosdroid.pojos.attack.Attack;
 
-class NsdConnectionManager extends ConnectionManager {
+class NsdConnectionManager extends ConnectionManager implements NsdManager.DiscoveryListener {
+    private static final String TAG = "NsdConnectionManager";
 
     NsdConnectionManager(Context context, Attack attack) {
         super(context, attack);
@@ -23,5 +27,35 @@ class NsdConnectionManager extends ConnectionManager {
     @Override
     protected void releaseResources() {
         super.releaseResources();
+    }
+
+    @Override
+    public void onDiscoveryStarted(String serviceType) {
+        Log.d(TAG, "Discovery started");
+    }
+
+    @Override
+    public void onDiscoveryStopped(String s) {
+        Log.d(TAG, "Discovery stopped");
+    }
+
+    @Override
+    public void onServiceFound(NsdServiceInfo nsdServiceInfo) {
+        Log.d(TAG, "Service found");
+    }
+
+    @Override
+    public void onServiceLost(NsdServiceInfo nsdServiceInfo) {
+        Log.d(TAG, "Service lost");
+    }
+
+    @Override
+    public void onStartDiscoveryFailed(String s, int errorCode) {
+        Log.d(TAG, "Start discovery failed");
+    }
+
+    @Override
+    public void onStopDiscoveryFailed(String s, int errorCode) {
+        Log.d(TAG, "Stop discovery failed");
     }
 }
