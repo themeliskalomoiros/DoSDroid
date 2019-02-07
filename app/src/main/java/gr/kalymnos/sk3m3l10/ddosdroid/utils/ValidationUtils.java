c03101b9d2d43a729ff9311b2f7142b1402b8e84
Tomas Michael <tomasmichael995@gmail.com>
2019-02-07 12:13:33 +0100

    Created ValidationUtils.getItemFrom()

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/ValidationUtils.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/ValidationUtils.java
@@ -2,22 +2,40 @@ package gr.kalymnos.sk3m3l10.ddosdroid.utils;
 
 import android.os.Bundle;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedSet;
 
 public final class ValidationUtils {
 
-    public static boolean listHasItems(List list) {
-        if (list != null && list.size() > 0) {
+    public static boolean collectionHasItems(Collection collection) {
+        if (collection != null && collection.size() > 0) {
             return true;
         }
         return false;
     }
 
-    public static boolean bundleIsValidAndContainsKey(Bundle bundle, String key) {
+    public static boolean bundleContains(Bundle bundle, String key) {
         if (bundle != null && bundle.containsKey(key)) {
             return true;
         }
         return false;
+    }
+
+    private static <T> T getItemFrom(LinkedHashSet<T> set, int index) {
+        if (index < 0 || index >= set.size())
+            throw new IllegalArgumentException("Invalid index: " + index + " while set's size is " + set.size() + ".");
+
+        T item = null;
+        int i = 0;
+        for (Iterator<T> iterator = set.iterator(); iterator.hasNext(); i++) {
+            item = iterator.next();
+            if (i == index)
+                break;
+        }
+        return item;
     }
 }
