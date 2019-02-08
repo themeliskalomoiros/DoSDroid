cbd5b592e6fd853cd0b4418546ff361d50ca1271
Tomas Michael <tomasmichael995@gmail.com>
2019-02-08 13:27:36 +0100

    Refactor: extracted WifiP2pServer.setHostInfoToAttack()

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/wifi_p2p/WifiP2pServer.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack/connectivity/server/wifi_p2p/WifiP2pServer.java
@@ -97,11 +97,15 @@ public class WifiP2pServer extends Server {
     }
 
     private void uploadAttack(WifiP2pGroup group) {
+        setHostInfoToAttack(group);
+        repository.upload(attack);
+    }
+
+    private void setHostInfoToAttack(WifiP2pGroup group) {
         WifiP2pDevice thisDevice = group.getOwner();
         attack.addSingleHostInfo(EXTRA_ATTACK_HOST_UUID, Bots.getLocalUser().getId());
         attack.addSingleHostInfo(EXTRA_DEVICE_NAME, thisDevice.deviceName);
         attack.addSingleHostInfo(EXTRA_MAC_ADDRESS, thisDevice.deviceAddress);
-        repository.upload(attack);
     }
 
     private void registerWifiDirectReceiver() {
