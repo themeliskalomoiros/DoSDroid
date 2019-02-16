f823cc7a21866b3e7d193d4a4a99ecd823813061
Tomas Michael <tomasmichael995@gmail.com>
2019-02-17 00:46:25 +0200

    Deleted the Wifi scanning process that was happening in InternetConstraint (removed also WifiScanResultsActivity and its ViewMvc)

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/connectivity/network_constraints/internet/InternetConstraint.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/connectivity/network_constraints/internet/InternetConstraint.java
@@ -1,112 +1,15 @@
 package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.connectivity.network_constraints.internet;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.ConnectivityManager;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
-import android.support.annotation.NonNull;
-import android.support.v4.content.LocalBroadcastManager;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities.WifiScanResultsActivity;
 import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.connectivity.network_constraints.NetworkConstraint;
 import gr.kalymnos.sk3m3l10.ddosdroid.utils.InternetConnectivity;
 
-import static android.net.wifi.WifiManager.SCAN_RESULTS_AVAILABLE_ACTION;
-import static gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities.WifiScanResultsActivity.ACTION_SCAN_RESULT_CANCELLED;
-import static gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities.WifiScanResultsActivity.ACTION_SCAN_RESULT_CHOSEN;
-import static gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities.WifiScanResultsActivity.ACTION_SCAN_RESULT_CONNECTION_ERROR;
-
 public class InternetConstraint extends NetworkConstraint {
-    private static final String TAG = "InternetConstraint";
-
-    private WifiManager wifiManager;
-    private BroadcastReceiver wifiScanReceiver, wifiConnectionReceiver;
 
     public InternetConstraint(Context context) {
         super(context);
-        initializeFields();
-    }
-
-    private void initializeFields() {
-        wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        initializeWifiScanReceiver();
-        initializeWifiConnectionReceiver();
-    }
-
-    private void initializeWifiScanReceiver() {
-        wifiScanReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                boolean resultsUpdated = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
-                if (resultsUpdated) {
-                    handleScanSuccess();
-                } else {
-                    handleScanFailure();
-                }
-            }
-
-            private void handleScanSuccess() {
-                registerWifiConnectionReceiver(context);
-                List<String> SSIDs = createSSIDsFrom(wifiManager.getScanResults());
-                WifiScanResultsActivity.startInstance(context, SSIDs);
-                unregisterWifiScanReceiver();
-            }
-
-            private void registerWifiConnectionReceiver(Context context) {
-                IntentFilter filter = createWifiConnectionReceiverIntentFilter();
-                LocalBroadcastManager manager = LocalBroadcastManager.getInstance(context);
-                manager.registerReceiver(wifiConnectionReceiver, filter);
-            }
-
-            @NonNull
-            private IntentFilter createWifiConnectionReceiverIntentFilter() {
-                IntentFilter filter = new IntentFilter();
-                filter.addAction(WifiScanResultsActivity.ACTION_SCAN_RESULT_CHOSEN);
-                filter.addAction(WifiScanResultsActivity.ACTION_SCAN_RESULT_CANCELLED);
-                filter.addAction(WifiScanResultsActivity.ACTION_SCAN_RESULT_CONNECTION_ERROR);
-                return filter;
-            }
-
-            @NonNull
-            private List<String> createSSIDsFrom(List<ScanResult> results) {
-                List<String> SSIDs = new ArrayList<>();
-                for (ScanResult result : results) {
-                    SSIDs.add(result.SSID);
-                }
-                return SSIDs;
-            }
-
-            private void handleScanFailure() {
-            }
-        };
-    }
-
-    private void initializeWifiConnectionReceiver() {
-        wifiConnectionReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                switch (intent.getAction()) {
-                    case ACTION_SCAN_RESULT_CHOSEN:
-                        onResolveConstraintListener.onConstraintResolved(context, InternetConstraint.this);
-                        break;
-                    case ACTION_SCAN_RESULT_CANCELLED:
-                        // TODO: implementation needed
-                        onResolveConstraintListener.onConstraintResolveFailed(context, InternetConstraint.this);
-                        break;
-                    case ACTION_SCAN_RESULT_CONNECTION_ERROR:
-                        // TODO: implementation needed
-                        break;
-                    default:
-                        throw new IllegalArgumentException(TAG + ": unknown action " + intent.getAction());
-                }
-            }
-        };
     }
 
     @Override
@@ -114,45 +17,13 @@ public class InternetConstraint extends NetworkConstraint {
         if (isResolved()) {
             onResolveConstraintListener.onConstraintResolved(context, this);
         } else {
-            registeWifiScanReceiver();
+            onResolveConstraintListener.onConstraintResolveFailed(context, this);
         }
-    }
-
-    private void registeWifiScanReceiver() {
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(SCAN_RESULTS_AVAILABLE_ACTION);
-        context.registerReceiver(wifiScanReceiver, filter);
     }
 
     @Override
     public boolean isResolved() {
         return isConnected();
-    }
-
-    @Override
-    public void releaseResources() {
-        super.releaseResources();
-        unregisterReceivers();
-    }
-
-    private void unregisterReceivers() {
-        unregisterWifiScanReceiver();
-        unregisterWifiConnectionReceiver();
-    }
-
-    private void unregisterWifiScanReceiver() {
-        if (wifiScanReceiver != null) {
-            context.unregisterReceiver(wifiScanReceiver);
-            wifiScanReceiver = null;
-        }
-    }
-
-    private void unregisterWifiConnectionReceiver() {
-        if (wifiConnectionReceiver != null) {
-            LocalBroadcastManager manager = LocalBroadcastManager.getInstance(context);
-            manager.unregisterReceiver(wifiConnectionReceiver);
-            wifiConnectionReceiver = null;
-        }
     }
 
     private boolean isConnected() {
