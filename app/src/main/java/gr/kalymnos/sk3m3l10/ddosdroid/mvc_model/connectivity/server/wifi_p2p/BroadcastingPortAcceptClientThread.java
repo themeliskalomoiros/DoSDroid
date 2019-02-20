1d6c2ba572db42accabbf30887f0a83630e58d1a
Tomas Michael <tomasmichael995@gmail.com>
2019-02-20 12:59:26 +0200

    Deleted /server/wifi_p2p/AcceptClientThread.java and created server/wifi_p2p/BroadcastingPortAcceptClientThread.java

---
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/connectivity/server/wifi_p2p/BroadcastingPortAcceptClientThread.java
@@ -0,0 +1,38 @@
+package gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.connectivity.server.wifi_p2p;
+
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+
+import gr.kalymnos.sk3m3l10.ddosdroid.constants.Extras;
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_model.connectivity.server.AcceptClientThread;
+
+/*  This thread first broadcasts the local port and then
+ *   starts accepting client connection request.*/
+
+public class BroadcastingPortAcceptClientThread extends AcceptClientThread {
+    public static final String ACTION_LOCAL_PORT_OBTAINED = "action local port obtained";
+
+    private int localPort;
+    private final LocalBroadcastManager manager;
+
+    public BroadcastingPortAcceptClientThread(ExecutorService executor, ServerSocket serverSocket, LocalBroadcastManager manager) {
+        super(executor, serverSocket);
+        this.manager = manager;
+        localPort = serverSocket.getLocalPort();
+    }
+
+    @Override
+    public void run() {
+        broadcast(localPort);
+        super.run();
+    }
+
+    private void broadcast(int port) {
+        Intent intent = new Intent(ACTION_LOCAL_PORT_OBTAINED);
+        intent.putExtra(Extras.EXTRA_LOCAL_PORT, port);
+        manager.sendBroadcast(intent);
+    }
+}
