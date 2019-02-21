0c30c3f4cc49e01298ff8664c118ba92789622d9
Tomas Michael <tomasmichael995@gmail.com>
2019-02-21 23:00:38 +0200

    Created DateFormatter.dateFrom(Time)

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/DateFormatter.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/DateFormatter.java
@@ -4,15 +4,15 @@ import android.support.annotation.NonNull;
 
 import java.util.Calendar;
 
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities.attack_creation.DatePicker;
+
 public final class DateFormatter {
 
     private DateFormatter() {
     }
 
-    public static String dateFrom(long timestamp) {
-        final Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(timestamp);
-        return dateFrom(cal);
+    public static String dateFrom(DatePicker.Date date) {
+        return dateFrom(date.year, date.month, date.dayOfMonth);
     }
 
     public static String dateFrom(int year, int month, int dayOfMonth) {
@@ -27,6 +27,12 @@ public final class DateFormatter {
         cal.set(Calendar.MONTH, month);
         cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
         return cal;
+    }
+
+    public static String dateFrom(long timestamp) {
+        final Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(timestamp);
+        return dateFrom(cal);
     }
 
     @NonNull
