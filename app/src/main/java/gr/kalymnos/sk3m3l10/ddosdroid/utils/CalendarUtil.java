05a13d87cc690152daa407881277db45530eed23
Tomas Michael <tomasmichael995@gmail.com>
2019-02-22 00:15:44 +0200

    Created CalendarUtil.from(DatePicker.Date date, TimePicker.Time time)

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/CalendarUtil.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/utils/CalendarUtil.java
@@ -4,9 +4,12 @@ import android.support.annotation.NonNull;
 
 import java.util.Calendar;
 
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities.attack_creation.DatePicker;
+import gr.kalymnos.sk3m3l10.ddosdroid.mvc_controllers.activities.attack_creation.TimePicker;
+
 public class CalendarUtil {
     @NonNull
-    static Calendar from(int year, int month, int dayOfMonth) {
+    public static Calendar from(int year, int month, int dayOfMonth) {
         final Calendar cal = Calendar.getInstance();
         cal.set(Calendar.YEAR, year);
         cal.set(Calendar.MONTH, month);
@@ -15,9 +18,19 @@ public class CalendarUtil {
     }
 
     @NonNull
-    static Calendar from(long timestamp) {
+    public static Calendar from(long timestamp) {
         final Calendar cal = Calendar.getInstance();
         cal.setTimeInMillis(timestamp);
+        return cal;
+    }
+
+    public static Calendar from(DatePicker.Date date, TimePicker.Time time) {
+        final Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, date.year);
+        cal.set(Calendar.MONTH, date.month);
+        cal.set(Calendar.DAY_OF_MONTH, date.dayOfMonth);
+        cal.set(Calendar.HOUR_OF_DAY, time.hourOfDay);
+        cal.set(Calendar.MINUTE, time.minute);
         return cal;
     }
 }
