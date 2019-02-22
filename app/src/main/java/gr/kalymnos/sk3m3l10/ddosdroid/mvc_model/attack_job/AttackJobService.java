840e73c79570a0572e926dbf833d3711fdea19a5
Tomas Michael <tomasmichael995@gmail.com>
2019-02-22 13:52:39 +0200

    Renamed to AttackJobService

--- app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack_job/AttackJobService.java
+++ app/src/main/java/gr/kalymnos/sk3m3l10/ddosdroid/mvc_model/attack_job/AttackJobService.java
@@ -5,15 +5,15 @@ import android.support.annotation.NonNull;
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
 
-public class AttackJob extends JobService {
+public class AttackJobService extends JobService {
 
     @Override
     public boolean onStartJob(@NonNull JobParameters job) {
-        return false;
+        return false; // Answers to the question: "Is there still work going on?"
     }
 
     @Override
     public boolean onStopJob(@NonNull JobParameters job) {
-        return false;
+        return false; // Answers to the question: "Should this job be retried?"
     }
 }
